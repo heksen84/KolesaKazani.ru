4914eea4890dda5100826c143fbdb1533bed3b22
Илья <heksen84@yandex.ru>
2018-05-27 12:39:46 +0600

    up+

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/newtrip', function () { return view('newtrip'); });
 Route::get('/categories', 'CategoriesController@index');
 Route::get('/search', function () { return view('search'); });
-Route::get('/category/{id}', 'CategoriesController@index');
+Route::get('/category/{id}', function () { return view('category'); });
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
