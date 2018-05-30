b4d4c3bf69557076b290832ef3701b7170a2a5b4
Илья <heksen84@yandex.ru>
2018-05-30 22:38:57 +0600

    up+

--- resources/assets/js/views/home.vue
+++ resources/assets/js/views/home.vue
@@ -1,5 +1,12 @@
 <template>
-	<b-button variant="success" @click="logout">выйти</b-button>
+	<b-container fluid>
+	<b-row>
+		<b-col cols="12" sm="12" md="9" lg="3" xl="3" style="text-align:center;margin:auto">
+			<h2>личный кабинет</h2>
+			<b-button variant="success" @click="logout">выйти</b-button>
+		</b-col>
+	</b-row>
+</b-container>
 </template>
 <script>
 import { get } from './../helpers/api'
