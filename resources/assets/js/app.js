0d6757adb0f75dc08897e6b2feda614c75fea1ac
Илья <heksen84@yandex.ru>
2018-06-02 16:23:41 +0600

    up+

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,29 +1,29 @@
 require('./bootstrap');
 
-import Vue from 'vue'
-import welcome from './views/welcome.vue'
-import home from './views/home.vue'
-import profile from './views/profile.vue'
-import search from './views/search.vue'
-import results from './views/results.vue'
-import create from './views/create.vue'
-import passwordreset from './views/auth/passwordreset.vue'
-import login from './views/auth/login.vue'
-import register from './views/auth/register.vue'
+import Vue from 'vue';
+import welcome from './views/welcome.vue';
+import home from './views/home.vue';
+import profile from './views/profile.vue';
+import search from './views/search.vue';
+import results from './views/results.vue';
+import create from './views/create.vue';
+import passwordreset from './views/auth/passwordreset.vue';
+import login from './views/auth/login.vue';
+import register from './views/auth/register.vue';
 
 
 // bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.min.css'
-import layout from 'bootstrap-vue/src/components/layout'
-import form from 'bootstrap-vue/src/components/form'
-import form_input from 'bootstrap-vue/src/components/form-input'
-import form_group from 'bootstrap-vue/src/components/form-group'
-import form_textarea from 'bootstrap-vue/src/components/form-textarea'
-import form_checkbox from 'bootstrap-vue/src/components/form-checkbox'
-import form_select from 'bootstrap-vue/src/components/form-select'
-import button from 'bootstrap-vue/src/components/button'
-import carousel from 'bootstrap-vue/src/components/carousel'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.min.css';
+import layout from 'bootstrap-vue/src/components/layout';
+import form from 'bootstrap-vue/src/components/form';
+import form_input from 'bootstrap-vue/src/components/form-input';
+import form_group from 'bootstrap-vue/src/components/form-group';
+import form_textarea from 'bootstrap-vue/src/components/form-textarea';
+import form_checkbox from 'bootstrap-vue/src/components/form-checkbox';
+import form_select from 'bootstrap-vue/src/components/form-select';
+import button from 'bootstrap-vue/src/components/button';
+import carousel from 'bootstrap-vue/src/components/carousel';
 
 Vue.use(layout);
 Vue.use(form);
