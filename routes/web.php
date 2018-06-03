64f5a9b8fe647a23c542fe3fdcd2becafb455bba
Илья <heksen84@yandex.ru>
2018-06-03 16:13:59 +0600

    up+

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+use Illuminate\Support\Facades\Auth;
+
 Auth::routes();
 Route::get('/', 'WelcomeController@getCategories');
 Route::get('/getUser', 'UserController@getUser');
@@ -6,6 +8,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/newtrip', function () { return view('newtrip'); });
 Route::get('/categories', 'CategoriesController@index');
 Route::get('/search', function () { return view('search'); });
-Route::get('/create', function () { return view('create'); });
+Route::get('/create', function () { return Auth::user()? view('create') : view('auth\login'); });
 Route::get('/category/{id}', function () { return view('category'); });
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
