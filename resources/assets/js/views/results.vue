ab8557410aa23c1defd292749321c95a0858d7d7
Илья <heksen84@yandex.ru>
2018-06-04 21:15:33 +0600

    up+

--- resources/assets/js/views/results.vue
+++ resources/assets/js/views/results.vue
@@ -1,9 +1,9 @@
 <template>
 	<b-container fluid>
 		<b-row>
-		<b-col cols="12" sm="12" md="9" lg="3" xl="3" style="text-align:center;margin:auto;margin-top:40px;color:grey">
-		<h1 style="color:white">результаты</h1>
-	</b-col>
+			<b-col cols="12" sm="12" md="9" lg="3" xl="3" style="text-align:center;margin:auto;margin-top:40px;color:grey">
+				<h1 style="color:white">результаты</h1>
+			</b-col>
 	</b-row>
 
 	<b-row style="margin-top:20px">
