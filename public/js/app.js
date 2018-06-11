cef76d8902b53dd3ef81821f6dd3281de934fada
Илья <heksen84@yandex.ru>
2018-06-12 00:10:21 +0600

    up+

--- public/js/app.js
+++ public/js/app.js
@@ -27962,7 +27962,7 @@ var render = function() {
                   "div",
                   {
                     staticStyle: {
-                      "font-size": "30px",
+                      "font-size": "31px",
                       "letter-spacing": "4px",
                       "font-weight": "450",
                       padding: "4px"
--- resources/assets/js/views/welcome.vue
+++ resources/assets/js/views/welcome.vue
@@ -14,7 +14,7 @@
     <b-row>
         <b-col cols="12" sm="12" md="12" lg="3" xl="3" style="text-align:center">
           <div id="logo_block">
-            <div style="font-size:30px;letter-spacing:4px;font-weight:450;padding:4px;">ЩЕПКА</div>
+            <div style="font-size:31px;letter-spacing:4px;font-weight:450;padding:4px;">ЩЕПКА</div>
             <div style="font-size:18px;color:yellow;margin-top:-12px;letter-spacing:2px;">доска объявлений</div>
           </div>
         </b-col>
