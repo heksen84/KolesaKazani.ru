39c9696f032fcc290dde0d58179cb17353a2e2cc
Илья <heksen84@yandex.ru>
2018-06-16 22:23:26 +0600

    up+

--- resources/assets/js/views/results.vue
+++ resources/assets/js/views/results.vue
@@ -6,23 +6,24 @@
 			</b-col>
 		</b-row>
 	<br>
-	<b-row style="margin-top:-2px" v-for="item in items" :key="item">
+	<b-row style="margin-top:1px" v-for="item in items" :key="item">
 		
 		<b-col cols="12" sm="12" md="2" lg="2" xl="2" :key="item.id">
 			<!--<item :id="item.id" :name="item.name"></item>-->
 
-<carousel :perPage=1 :paginationEnabled=false>
+
+<carousel :perPage=1 :paginationEnabled="false">
   <slide>
-     <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image" />
+     <b-img fluid src="https://picsum.photos/250/250/?image=54" fluid-grow alt="Fluid-Grow image" />
   </slide>
   <slide>
-     <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image" />
+      <b-img fluid src="https://picsum.photos/250/250/?image=54" fluid-grow alt="Fluid-Grow image"/>
   </slide>
   <slide>
-     <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image" />
+      <b-img fluid src="https://picsum.photos/250/250/?image=54" fluid-grow alt="Fluid-Grow image"/>
   </slide>
   <slide>
-     <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image" />
+     <b-img fluid src="https://picsum.photos/250/250/?image=54" fluid-grow alt="Fluid-Grow image"/>
   </slide>
 </carousel>
 
@@ -53,3 +54,15 @@ export default {
 	}
 }
 </script>
+
+<style>
+.VueCarousel-slide {
+  position: relative;
+  background: #42b983;
+  color: #fff;
+  font-family: Arial;
+  font-size: 24px;
+  text-align: center;
+ 
+}
+</style>
