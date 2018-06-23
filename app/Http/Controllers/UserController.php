96e86e35118a8215137cf5725c137ff31386ca64
Илья <heksen84@yandex.ru>
2018-06-23 17:09:04 +0600

    up+

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,12 +7,6 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller {
       public function getUser(Request $request) {
-      	//if (!Auth::guest()) 
       		return Auth::user();
-      	/*$user = Session::get('userData');
-		if($user->id) 
-			echo 'user is logged-in';
-		else 
-			echo 'guest only privilegies';*/
     }
 }
