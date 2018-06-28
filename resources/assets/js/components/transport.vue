1fd1cbe6d4a9249519dbf1b51d9d83705a373f4d
Илья <heksen84@yandex.ru>
2018-06-28 22:31:04 +0600

    up+

---
+++ resources/assets/js/components/transport.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="item" @click="details">
+    <!--<avatarka></avatarka>-->
+    <h4 style="margin:15px;color:rgb(70,70,70)">{{ title }}</h4>
+    <div style="margin:15px;color:rgb(70,70,70)">{{ text }}</div>
+    <h5 style="margin:15px;color:rgb(70,70,70)">цена: {{ price }}</h5>
+  </div>
+</template>
+
+<script>
+import avatarka from "./avatarka"
+export default {
+  props: ['id', 'photo', 'title', 'text', 'price'],
+  data () {
+    return 	{
+		}
+	},
+  created() {
+  },
+  components: { avatarka },
+  methods: {
+    details(event) {
+        window.location='/details/'+this.id;  
+    }
+  }
+}
+</script>
