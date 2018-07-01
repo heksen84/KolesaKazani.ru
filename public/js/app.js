c4da6fbbae84e53fd3f925856cbfc1572b4c8e38
Илья <heksen84@yandex.ru>
2018-07-01 17:31:54 +0600

    up+

--- public/js/app.js
+++ public/js/app.js
@@ -1742,7 +1742,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
-//
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
@@ -28144,7 +28143,7 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", { staticClass: "item", on: { click: _vm.details } }, [
-    _c("h4", { staticStyle: { margin: "15px", color: "rgb(70,70,70)" } }, [
+    _c("h5", { staticStyle: { margin: "15px", color: "rgb(70,70,70)" } }, [
       _vm._v(_vm._s(_vm.title))
     ]),
     _vm._v(" "),
@@ -28152,7 +28151,7 @@ var render = function() {
       _vm._v(_vm._s(_vm.text))
     ]),
     _vm._v(" "),
-    _c("h5", { staticStyle: { margin: "15px", color: "rgb(70,70,70)" } }, [
+    _c("h6", { staticStyle: { margin: "15px", color: "rgb(70,70,70)" } }, [
       _vm._v("цена: " + _vm._s(_vm.price))
     ])
   ])
--- resources/assets/js/components/item.vue
+++ resources/assets/js/components/item.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="item" @click="details">
-    <!--<avatarka></avatarka>-->
-    <h4 style="margin:15px;color:rgb(70,70,70)">{{ title }}</h4>
+    <h5 style="margin:15px;color:rgb(70,70,70)">{{ title }}</h5>
     <div style="margin:15px;color:rgb(70,70,70)">{{ text }}</div>
-    <h5 style="margin:15px;color:rgb(70,70,70)">цена: {{ price }}</h5>
+    <h6 style="margin:15px;color:rgb(70,70,70)">цена: {{ price }}</h6>
   </div>
 </template>
 
