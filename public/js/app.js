9c7c99cc67be8c6fb83ac5d0de5d4650a86bb4ad
Ilya <heksen84@yandex.ru>
2018-07-30 12:50:28 +0600

    up+

--- public/js/app.js
+++ public/js/app.js
@@ -46873,10 +46873,8 @@ if (token) {
 
 "use strict";
 /* harmony default export */ __webpack_exports__["a"] = ({
-
-    options_sdelka: [{ value: '0', text: 'Покупка' }, { value: '1', text: 'Продажа' }, { value: '2', text: 'Обмен' }, { value: '3', text: 'Частичный обмен' }, { value: '4', text: 'Отдам даром' }, { value: '5', text: 'Сдача в аренду' }],
-
-    max_load_images: 20
+    max_load_images: 20,
+    options_sdelka: [{ value: '0', text: 'Покупка' }, { value: '1', text: 'Продажа' }, { value: '2', text: 'Обмен' }, { value: '3', text: 'Частичный обмен' }, { value: '4', text: 'Отдам даром' }, { value: '5', text: 'Сдача в аренду' }]
 });
 
 /***/ }),
--- resources/assets/js/common.js
+++ resources/assets/js/common.js
@@ -1,14 +1,12 @@
 export default {
-	
-     options_sdelka: 
-     [
+	max_load_images: 20,
+    options_sdelka: 
+    [
         { value: '0', text: 'Покупка' },
         { value: '1', text: 'Продажа' },
         { value: '2', text: 'Обмен' },
         { value: '3', text: 'Частичный обмен' },
         { value: '4', text: 'Отдам даром' },
         { value: '5', text: 'Сдача в аренду' }
-    ],
-
-    max_load_images: 20
+    ]
 }
