18f1ed9ba6f058fae022c1c7067b8a4ba994f44d
Ilya <heksen84@yandex.ru>
2018-08-08 18:31:35 +0600

    up+

--- public/js/app.js
+++ public/js/app.js
@@ -33836,7 +33836,7 @@ var render = function() {
                 [
                   _c("b-form-select", {
                     staticClass: "mb-3",
-                    attrs: { options: _vm.options_price, size: "sm" },
+                    attrs: { options: _vm.options_price },
                     on: { change: _vm.getSearchData },
                     model: {
                       value: _vm.filters.price,
@@ -33856,7 +33856,7 @@ var render = function() {
                 [
                   _c("b-form-select", {
                     staticClass: "mb-3",
-                    attrs: { options: _vm.options_sdelka, size: "sm" },
+                    attrs: { options: _vm.options_sdelka },
                     on: { change: _vm.getSearchData },
                     model: {
                       value: _vm.filters.sdelka,
@@ -33876,7 +33876,7 @@ var render = function() {
                 [
                   _c("b-form-select", {
                     staticClass: "mb-3",
-                    attrs: { options: _vm.options_location, size: "sm" },
+                    attrs: { options: _vm.options_location },
                     on: { change: _vm.getSearchData },
                     model: {
                       value: _vm.filters.location,
