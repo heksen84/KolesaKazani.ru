91bb741340e46ef1fa8a752a615fc6cf7612a504
Ilya <heksen84@yandex.ru>
2018-08-11 20:28:04 +0600

    up+

--- public/js/app.js
+++ public/js/app.js
@@ -33437,7 +33437,7 @@ var render = function() {
                   "div",
                   {
                     staticStyle: {
-                      "font-size": "31px",
+                      "font-size": "33px",
                       "letter-spacing": "4px",
                       "font-weight": "450",
                       padding: "4px",
