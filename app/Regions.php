02cd53f912e9c257501d0ecfca91a868686de781
Ilya <heksen84@yandex.ru>
2018-08-23 21:06:24 +0600

    up+

--- app/Regions.php
+++ app/Regions.php
@@ -6,9 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Regions extends Model
 {
-    
     protected $table = 'kz_region';
 	protected $primaryKey = 'region_id';
 	public $timestamps = false;
-
 }
