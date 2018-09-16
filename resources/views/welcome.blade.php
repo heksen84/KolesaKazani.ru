08eb887a44996ea65654cc100bf5959cb25b47fc
Ilya <heksen84@yandex.ru>
2018-09-16 21:59:33 +0600

    up+

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="keywords" content="get numbers, free numbers" />
-  <meta name="description" content="АксуМаркет - сайт объявлений">
+  <meta name="description" content="КупиПродай - сайт объявлений">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Доска объявлений АксуМаркет. Частные объявления о покупке и продаже.</title>
+  <title>Доска объявлений КупиПродай. Частные объявления о покупке и продаже.</title>
   <link rel="icon" href="{{ asset('public/shop.ico') }}">
 </head>
 <body>
