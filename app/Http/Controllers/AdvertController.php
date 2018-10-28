88c0038616860bf91b16a9e787764ce90579f9eb
Ilya <heksen84@yandex.ru>
2018-10-28 16:13:54 +0600

    ok

--- app/Http/Controllers/AdvertController.php
+++ app/Http/Controllers/AdvertController.php
@@ -64,7 +64,6 @@ class AdvertController extends Controller
         if ( $validator->fails() )  
             return response()->json(["result"=>"usr.error", "msg"=>$validator->errors()->first()]);                    
 
-                
         $category   = $data["adv_category"];
         $text       = $data["adv_info"];
         $price      = $data["adv_price"];
