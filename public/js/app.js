19ed70fcfbb4398731ab1c0d6208b5cc250b4bc5
Ilya <heksen84@yandex.ru>
2018-10-28 11:28:59 +0600

    щл

--- public/js/app.js
+++ public/js/app.js
@@ -2555,12 +2555,22 @@ function forEach(data, callback) {
 	},
 	created: function created() {
 
+		var myMap, myPlacemark;
+
 		function init() {
-			var myMap = new ymaps.Map("map", {
+			myMap = new ymaps.Map("map", {
 				center: [55.76, 37.64],
 				zoom: 10
 			});
+
+			myPlacemark = new ymaps.Placemark([55.76, 37.64], {
+				hintContent: 'Москва!',
+				balloonContent: 'Столица России'
+			});
+
+			myMap.geoObjects.add(myPlacemark);
 		}
+
 		ymaps.ready(init);
 
 		this.$root.advert_data.adv_info = null; // добавляю формально поле доп. информация
--- resources/assets/js/views/create.vue
+++ resources/assets/js/views/create.vue
@@ -159,13 +159,23 @@ export default {
 	},
 
 	created() {
-		
-		 function init(){     
-        	var myMap = new ymaps.Map ("map", {
+
+		var myMap, myPlacemark;
+		 
+		function init() {     
+        	myMap = new ymaps.Map ("map", {
             center: [55.76, 37.64],
            	zoom: 10
+		});
+		
+		myPlacemark = new ymaps.Placemark([55.76, 37.64], { 
+            hintContent: 'Москва!', 
+            balloonContent: 'Столица России' 
         });
-	}
+
+		myMap.geoObjects.add(myPlacemark);
+		}
+
 		ymaps.ready(init);
 
 		this.$root.advert_data.adv_info = null; // добавляю формально поле доп. информация
