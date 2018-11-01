8e5941004ac08f62b283776fef4772fdbe600ba9
Ilya <heksen84@yandex.ru>
2018-11-01 20:24:48 +0600

    ok

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -9,7 +9,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Доска объявлений Дамеля</title>
-  <link rel="icon" href="{{ asset('public/globe.ico') }}">
+  <link rel="icon" href="{{ asset('public/shop.ico') }}">
   <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 </head>
 <body>
