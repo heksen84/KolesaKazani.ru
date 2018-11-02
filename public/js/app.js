9fc28e56d2c20e64cb6f66bdccf4a744ba441879
Ilya Bobkov <Ilya.bobkov@erg.kz>
2018-11-02 16:36:03 +0600

    +

--- public/js/app.js
+++ public/js/app.js
@@ -35111,8 +35111,8 @@ var render = function() {
               "margin-top": "10px",
               "font-weight": "500",
               padding: "5px",
-              background: "rgb(120,120,180)",
-              opacity: "0.9",
+              background: "rgb(120,120,160)",
+              opacity: "0.8",
               "font-size": "15px",
               width: "100%",
               "text-align": "center"
