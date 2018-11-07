4acbb7721525d57baf37fcdd3e126ff314362d99
Ilya <heksen84@yandex.ru>
2018-11-07 20:21:40 +0600

    ok

--- app/Notifications/MailResetPasswordNotification.php
+++ app/Notifications/MailResetPasswordNotification.php
@@ -7,36 +7,22 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class MailResetPasswordNotification extends Notification
-{
+class MailResetPasswordNotification extends Notification {
     use Queueable;
 
     protected $token;
 
-    /**
-     * Create a new notification instance.
-     *
-     * @return void
-     */
     public function __construct($token)
     {
         $this->token=$token;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
     public function via($notifiable)
     {
         return ['mail'];
     }
 
-   
     public function toMail( $notifiable ) {
-
     $link = url( "/password/reset/".$this->token);
 
     return ( new MailMessage )
@@ -45,7 +31,6 @@ class MailResetPasswordNotification extends Notification
       ->line( "Для подтверждения сброса пароля нажмите на кнопку 'Сбросить пароль'" )
       ->action( 'Сбросить пароль', $link )
       ->line( 'Спасибки!' );
-
     }
     
     public function toArray($notifiable) {
