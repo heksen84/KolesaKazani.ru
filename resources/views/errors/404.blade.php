97cb60f534386eb42e429e01dec1061256e822e7
Ilya Bobkov <Ilya.bobkov@erg.kz>
2018-11-15 17:26:39 +0600

    -

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,7 +10,7 @@
 	<link rel="icon" href="{{ asset('public/shop.ico') }}">
     </head>
 <body>
-	<h1 style="text-align:center;margin-top:30px;color:rgb(100,100,130)">Страница не найдена</h1>
+	<h1 style="text-align:center;margin-top:30px;color:rgb(100,100,130)">Такой страницы нет</h1>
     	<h4 style="text-align:center"><a href="/" style="color:rgb(100,100,100);text-decoration:none">вернуться на главную</a></h4>
 </body>
 </html>
