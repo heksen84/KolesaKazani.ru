b1ef87cbfc5b0155c5d7c72b9a5f210559dfd0f9
Ilya Bobkov <Ilya.bobkov@erg.kz>
2019-01-17 11:12:16 +0600

    -

--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,37 @@ use Illuminate\Support\Facades\DB;
 
 Auth::routes();
 
-//  ------------ TEST ---------------
+// ------------------------------------
+//  Пробный методы
+// ------------------------------------
 Route::get('test',  function () { return view('test'); });
 Route::post('checkPhotos',  'TestController@checkPhotos');
 
+// ------------------------------------
 // Модерация
+// ------------------------------------
 Route::get('moderation',  function () { return view('moderation'); });
 Route::get('moderation/{advert_id}',  function () { return view('moderation_advert'); });
 
 //Route::get('details/{id}', array('as' => 'id', 'uses' => 'AdvertController@getFullInfo'));
 
+// ------------------------------------
 // Реклама на сайте
+// ------------------------------------
 Route::get('advertisers',  function () { return view('advertisers'); });
+
 
 Route::get('getCategoryCountById', 'WelcomeController@getCategoryCountById');
 Route::get('getCategoryCounts', 'WelcomeController@getCategoryCounts');
 
+// ------------------------------------
 // сервисы
+// ------------------------------------
 Route::get('/util/str2url', 'UtilsController@str2url');
 
+// ------------------------------------
 // категории по всему Казахстану
+// ------------------------------------
 Route::get('transport', 				'ResultsController@getResultsByCategory');
 //Route::get('transport', 				'SubCatController@showSubCategory');
 Route::get('nedvizhimost', 				'ResultsController@getResultsByCategory');
@@ -35,9 +46,35 @@ Route::get('hobbi-i-otdyh', 			'ResultsController@getResultsByCategory');
 Route::get('uslugi', 					'ResultsController@getResultsByCategory');
 Route::get('drugoe', 					'ResultsController@getResultsByCategory');
 
+// ------------------------------------
+// подкатегории
+// ------------------------------------
+// транспорт
+Route::get('gruzovoy-avtomobil', 	  		'ResultsController@getResultsByCategory');
+Route::get('legkovoy-avtomobil', 	  		'ResultsController@getResultsByCategory');
+Route::get('mototehnika', 			  		'ResultsController@getResultsByCategory');
+Route::get('spectehnika', 			  		'ResultsController@getResultsByCategory');
+Route::get('retro-avtomobil', 		  		'ResultsController@getResultsByCategory');
+Route::get('vodnyy-transport', 		  		'ResultsController@getResultsByCategory');
+Route::get('velosiped', 			  		'ResultsController@getResultsByCategory');
+Route::get('vozdushnyy-transport',	  		'ResultsController@getResultsByCategory');
+// недвижимость
+Route::get('kvartira', 				  		'ResultsController@getResultsByCategory');
+Route::get('komnata', 				  		'ResultsController@getResultsByCategory');
+Route::get('dom-dacha-kottedzh',	  		'ResultsController@getResultsByCategory');
+Route::get('zemel-nyy-uchastok',	  		'ResultsController@getResultsByCategory');
+Route::get('garazh-ili-mashinomesto', 		'ResultsController@getResultsByCategory');
+Route::get('kommercheskaya-nedvizhimost', 	'ResultsController@getResultsByCategory');
+Route::get('nedvizhimost-za-rubezhom', 		'ResultsController@getResultsByCategory');
+
+// ------------------------------------
+// детали объявления
+// ------------------------------------
 Route::get('details/{id}', ['uses' => 'AdvertController@getFullInfo']);
 
+// ------------------------------------
 // базовые контроллеры
+// ------------------------------------
 Route::post('create', 			 'AdvertController@createAdvert');
 Route::get('podat-obyavlenie', 	 'AdvertController@newAdvert');
 Route::get('/', 				 'IndexController@getCategories');
@@ -48,17 +85,19 @@ Route::get('home',	 		 	 'CabinetController@index');
 Route::get('home/{advert_id}',	 'CabinetController@index');
 Route::get('categories', 		 'CategoriesController@index');
 
-
 Route::get('getSubCats',  'SubCatsController@getSubCats' );
 
+// ------------------------------------
 // авто
+// ------------------------------------
 Route::get('getCarsMarks',  'AdvertController@getCarsMarks' );
 Route::get('getCarsModels', 'AdvertController@getCarsModels' );
 
 Route::get('search',  function () { return view('search')->with("items", "123"); });
 
-
+// ------------------------------------
 // перенести в контроллер Categories
+// ------------------------------------
 Route::get('/category/{id}', function ($id) { 
 	$categories = DB::table('categories')->where('id', $id)->get();
 	$items = DB::table('adverts')->where('category_id', $id)->get();
@@ -66,14 +105,18 @@ Route::get('/category/{id}', function ($id) {
 		mb_strtolower($categories[0]->name));
 });
 
-
 Route::get('getSearchData', 'SearchController@getSearchData');
 Route::get('location/{country}/{region}/{place}', ['uses' => 'AdvertController@getFullInfo']);
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
+// ------------------------------------
 // категории по региону
+// ------------------------------------
 Route::get('{region}/{category}', 'ResultsController@getResultsByRegion');
+
+// ------------------------------------
 // категории по региону и местности
+// ------------------------------------
 Route::get('{region}/{place}/{category}', 'ResultsController@getResultsByPlace');
 
 
