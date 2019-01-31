f7c029ce94ab643725a59d6220801453d200663b
Ilya <heksen84@yandex.ru>
2019-01-31 19:20:23 +0600

    -

--- resources/assets/js/views/create.vue
+++ resources/assets/js/views/create.vue
@@ -89,8 +89,8 @@
 
 				<b-form-group label="Контактные номера:" style="text-align:center;font-weight:bold;text-decoration:underline">
 			 		<b-form-input type="text" placeholder="Введите номер 1" style="width:280px;display:inline;text-align:center"   :formatter="setPhoneNumber" required></b-form-input>
-					<b-form-input type="text" placeholder="Введите номер 2 (не обязательно)" style="width:280px;text-align:center;margin: 5px auto" :formatter="setPhoneNumber"></b-form-input>
-					<b-form-input type="text" placeholder="Введите номер 3 (не обязательно)" style="width:280px;text-align:center;margin: 5px auto" :formatter="setPhoneNumber"></b-form-input>
+					<b-form-input type="text" placeholder="Введите номер 2" style="width:280px;text-align:center;margin: 5px auto" :formatter="setPhoneNumber"></b-form-input>
+					<b-form-input type="text" placeholder="Введите номер 3" style="width:280px;text-align:center;margin: 5px auto" :formatter="setPhoneNumber"></b-form-input>
 				</b-form-group>
 
 				<!-- Город, Село и т.д. -->
