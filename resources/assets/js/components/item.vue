0d0c56195e8a20994825f02d2fb709d895815061
Ilya <heksen84@yandex.ru>
2019-02-13 21:38:07 +0600

    -

--- resources/assets/js/components/item.vue
+++ resources/assets/js/components/item.vue
@@ -1,11 +1,10 @@
 <template>
 
   <div class="item" @click="details">
-    
     <b-img :src="'../storage/app/images/'+image" width="118" height="118" style="display:inline-block" v-if="image!=null"/>
     <b-img :src="'../public/images/_nofoto.jpg'" width="118" height="118" style="display:inline-block" v-if="image==null"/>
 
-  <div class="item-content">      
+    <div class="item-content">      
       <b>{{ title }}</b>
         <div>Цена: {{ price }} тенге</div>
       <b-button style="margin-top:5px" size="sm" variant="outline-success">Подробнее</b-button>
