25f8d93db3e619e5276220c571ec5540fb559885
Ilya <heksen84@yandex.ru>
2019-02-13 21:52:10 +0600

    -

--- resources/assets/js/components/item.vue
+++ resources/assets/js/components/item.vue
@@ -1,31 +1,18 @@
 <template>
-
   <div class="item" @click="details">
     <b-img :src="'../storage/app/images/'+image" width="118" height="118" style="display:inline-block" v-if="image!=null"/>
     <b-img :src="'../public/images/_nofoto.jpg'" width="118" height="118" style="display:inline-block" v-if="image==null"/>
-
     <div class="item-content">      
       <b>{{ title }}</b>
         <div>Цена: {{ price }} тенге</div>
       <b-button style="margin-top:5px" size="sm" variant="outline-success">Подробнее</b-button>
     </div>
   </div>
-    
 </template>
-
 <script>
 export default {
-  props: [ "id", "image", "title", "price", "category_id" ],
-
-  data () {
-    return 	{}
-	},
-  
-  created() {
-    console.log("картинка :"+this.image)
-  },
-
-  methods: {
+props: [ "id", "image", "title", "price", "category_id" ],
+methods: {
     details(event) { window.location='/details/'+this.id;  
     }
   }
