b9b3f21cdfce2f7cca148052c8b233f162a4740a
Ilya Bobkov <Ilya.bobkov@erg.kz>
2019-02-19 10:00:23 +0600

    -

--- resources/assets/js/components/item.vue
+++ resources/assets/js/components/item.vue
@@ -4,7 +4,8 @@
     <b-img :src="'../public/images/_nofoto.jpg'" width="118" height="118" style="display:inline-block" v-if="image==null"/>
     <div class="item-content">      
       <b>{{ title }}</b>
-        <div>Цена: {{ price }} тенге</div>
+        <div v-if="category_id!=4">Цена: {{ price }} тенге</div>
+        <br v-if="category_id= =4">
       <b-button style="margin-top:5px" size="sm" variant="outline-success">Подробнее</b-button>
     </div>
   </div>
