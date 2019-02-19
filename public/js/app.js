a972ecdd0adab89f9814587f9024594fb04aec18
Ilya Bobkov <Ilya.bobkov@erg.kz>
2019-02-19 10:03:50 +0600

    -

--- public/js/app.js
+++ public/js/app.js
@@ -2115,6 +2115,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
     props: ["id", "image", "title", "price", "category_id"],
--- resources/assets/js/components/item.vue
+++ resources/assets/js/components/item.vue
@@ -4,7 +4,9 @@
     <b-img :src="'../public/images/_nofoto.jpg'" width="118" height="118" style="display:inline-block" v-if="image==null"/>
     <div class="item-content">      
       <b>{{ title }}</b>
+        <!-- не отображать цену в услугах (category_id==4) -->
         <div v-if="category_id!=4">Цена: {{ price }} тенге</div>
+        <!-- сделать перенос на след. строку в услугах (category_id==4) -->
         <br v-if="category_id==4">
       <b-button style="margin-top:5px" size="sm" variant="outline-success">Подробнее</b-button>
     </div>
