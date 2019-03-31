a5f8ac181d145c1aea6b8666fadcae4480c7c46c
Ilya <heksen84@yandex.ru>
2019-03-31 18:14:24 +0600

    -

--- resources/assets/js/views/adminpanel.vue
+++ resources/assets/js/views/adminpanel.vue
@@ -6,10 +6,10 @@
     <h1 class="form_title">модератор</h1>    
     <hr>
     <b-form @submit="onSubmit">
-		<b-form-input type="text" maxlength="12" placeholder="логин" class="mb-1 mr-sm-1 mb-sm-1"></b-form-input>
-		<b-form-input type="password" maxlength="12" placeholder="пароль" class="mb-1 mr-sm-1 mb-sm-1"></b-form-input>
-		<b-form-group style="text-align:center;margin:25px">
-		<b-button type="onSubmit" variant="outline-primary" title="Опубликовать объявление">войти</b-button>
+			<b-form-input type="text" maxlength="12" placeholder="логин" class="mb-1 mr-sm-1 mb-sm-1"></b-form-input>
+			<b-form-input type="password" maxlength="12" placeholder="пароль" class="mb-1 mr-sm-1 mb-sm-1"></b-form-input>
+			<b-form-group style="text-align:center;margin:25px">
+			<b-button type="onSubmit" variant="outline-primary" title="Опубликовать объявление">войти</b-button>
 		</b-form-group>
 	</b-form>
 	</b-col>
