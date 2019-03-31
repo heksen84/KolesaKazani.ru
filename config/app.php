fb4fe6131ea0e2ed0b2edfdb87831edc273c5060
Ilya <heksen84@yandex.ru>
2019-03-31 15:46:38 +0600

    -

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', true),
+    'debug' => env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -227,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-	'Debugbar' => Barryvdh\Debugbar\Facade::class
+	//'Debugbar' => Barryvdh\Debugbar\Facade::class
 
     ],
 
