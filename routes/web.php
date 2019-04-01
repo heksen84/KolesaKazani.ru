cac933d38ed525bd9a851ba905749336f41a0dbe
Ilya Bobkov <Ilya.bobkov@erg.kz>
2019-04-01 15:46:40 +0600

    -

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\DB;
 
 Auth::routes(); // Стандартные роуты
 
-//if (env('APP_DEBUG')) {
+//if (env("APP_DEBUG")) {
 	//\Debugbar::disable();
 	\Debugbar::enable();
 //}
@@ -31,81 +31,81 @@ Route::get("getCategoryCounts", "WelcomeController@getCategoryCounts");
 // -----------------------------------------------------------------------------------------
 // Категории по всему Казахстану
 // -----------------------------------------------------------------------------------------
-Route::get('transport', 		'ResultsController@getResultsByCategoryForView');
-Route::get('nedvizhimost', 		'ResultsController@getResultsByCategoryForView');
-Route::get('elektronika', 		'ResultsController@getResultsByCategoryForView');
-Route::get('rabota-i-biznes', 	'ResultsController@getResultsByCategoryForView');
-Route::get('dlya-doma-i-dachi',	'ResultsController@getResultsByCategoryForView');
-Route::get('lichnye-veschi', 	'ResultsController@getResultsByCategoryForView');
-Route::get('zhivotnye', 		'ResultsController@getResultsByCategoryForView');
-Route::get('hobbi-i-otdyh', 	'ResultsController@getResultsByCategoryForView');
-Route::get('uslugi', 			'ResultsController@getResultsByCategoryForView');
-Route::get('drugoe', 			'ResultsController@getResultsByCategoryForView');
+Route::get("transport", 		"ResultsController@getResultsByCategoryForView");
+Route::get("nedvizhimost", 		"ResultsController@getResultsByCategoryForView");
+Route::get("elektronika", 		"ResultsController@getResultsByCategoryForView");
+Route::get("rabota-i-biznes", 	"ResultsController@getResultsByCategoryForView");
+Route::get("dlya-doma-i-dachi",	"ResultsController@getResultsByCategoryForView");
+Route::get("lichnye-veschi", 	"ResultsController@getResultsByCategoryForView");
+Route::get("zhivotnye", 		"ResultsController@getResultsByCategoryForView");
+Route::get("hobbi-i-otdyh", 	"ResultsController@getResultsByCategoryForView");
+Route::get("uslugi", 			"ResultsController@getResultsByCategoryForView");
+Route::get("drugoe", 			"ResultsController@getResultsByCategoryForView");
 
-Route::get('{region}/transport/{subcat}',	 ['uses' => 'ResultsController@getResultsByRegionWithDetailedInfo']);
-Route::get('{region}/nedvizhimost/{subcat}', ['uses' => 'ResultsController@getResultsByRegionWithDetailedInfo']);
+Route::get("{region}/transport/{subcat}",	 ["uses" => "ResultsController@getResultsByRegionWithDetailedInfo"]);
+Route::get("{region}/nedvizhimost/{subcat}", ["uses" => "ResultsController@getResultsByRegionWithDetailedInfo"]);
 
 // по региону без подкатегорий 
-Route::get('{region}/elektronika',  		 ['uses' => 'ResultsController@getResultsByRegion']);
-Route::get('{region}/rabota-i-biznes',  	 ['uses' => 'ResultsController@getResultsByRegion']);
-Route::get('{region}/dlya-doma-i-dachi',  	 ['uses' => 'ResultsController@getResultsByRegion']);
-Route::get('{region}/lichnye-veschi',  	 	 ['uses' => 'ResultsController@getResultsByRegion']);
-Route::get('{region}/zhivotnye',  	 	 	 ['uses' => 'ResultsController@getResultsByRegion']);
-Route::get('{region}/hobbi-i-otdyh',  	 	 ['uses' => 'ResultsController@getResultsByRegion']);
-Route::get('{region}/uslugi',  	 	 		 ['uses' => 'ResultsController@getResultsByRegion']);
-Route::get('{region}/drugoe',  	 	 		 ['uses' => 'ResultsController@getResultsByRegion']);
+Route::get("{region}/elektronika",  		 ["uses" => "ResultsController@getResultsByRegion"]);
+Route::get("{region}/rabota-i-biznes",  	 ["uses" => "ResultsController@getResultsByRegion"]);
+Route::get("{region}/dlya-doma-i-dachi",  	 ["uses" => "ResultsController@getResultsByRegion"]);
+Route::get("{region}/lichnye-veschi",  	 	 ["uses" => "ResultsController@getResultsByRegion"]);
+Route::get("{region}/zhivotnye",  	 	 	 ["uses" => "ResultsController@getResultsByRegion"]);
+Route::get("{region}/hobbi-i-otdyh",  	 	 ["uses" => "ResultsController@getResultsByRegion"]);
+Route::get("{region}/uslugi",  	 	 		 ["uses" => "ResultsController@getResultsByRegion"]);
+Route::get("{region}/drugoe",  	 	 		 ["uses" => "ResultsController@getResultsByRegion"]);
 
 // недвижимость
-Route::get('kvartira', 				  		'ResultsController@getResultsByCategory');
-Route::get('komnata', 				  		'ResultsController@getResultsByCategory');
-Route::get('dom-dacha-kottedzh',	  		'ResultsController@getResultsByCategory');
-Route::get('zemel-nyy-uchastok',	  		'ResultsController@getResultsByCategory');
-Route::get('garazh-ili-mashinomesto', 		'ResultsController@getResultsByCategory');
-Route::get('kommercheskaya-nedvizhimost', 	'ResultsController@getResultsByCategory');
-Route::get('nedvizhimost-za-rubezhom', 		'ResultsController@getResultsByCategory');
+Route::get("kvartira", 				  		"ResultsController@getResultsByCategory");
+Route::get("komnata", 				  		"ResultsController@getResultsByCategory");
+Route::get("dom-dacha-kottedzh",	  		"ResultsController@getResultsByCategory");
+Route::get("zemel-nyy-uchastok",	  		"ResultsController@getResultsByCategory");
+Route::get("garazh-ili-mashinomesto", 		"ResultsController@getResultsByCategory");
+Route::get("kommercheskaya-nedvizhimost", 	"ResultsController@getResultsByCategory");
+Route::get("nedvizhimost-za-rubezhom", 		"ResultsController@getResultsByCategory");
 
 // ------------------------------------
 // детали объявления
 // ------------------------------------
-Route::get('obyavlenie/{url}', 'AdvertController@getFullInfoByUrl'); // для СЕО
-Route::get('podrobno/{id}', 'AdvertController@getFullInfo');
+Route::get("obyavlenie/{url}", "AdvertController@getFullInfoByUrl"); // для СЕО
+Route::get("podrobno/{id}", "AdvertController@getFullInfo");
 
 // подкатегории
-Route::get('{category}/{subcat}', 'ResultsController@getResultsForSubCategoryForView');
-//Route::get('/getResultsForSubCategory', 'ResultsController@getResultsForSubCategory'); // для axios
+Route::get("{category}/{subcat}", "ResultsController@getResultsForSubCategoryForView");
+//Route::get("/getResultsForSubCategory", "ResultsController@getResultsForSubCategory"); // для axios
 
-Route::get('/getResultsForSubCategory/{category}/{subcat}', 'ResultsController@getResultsForSubCategoryForView');
-Route::get('/getResultsByCategoryForFront', 'ResultsController@getResultsByCategoryForFront');
+Route::get("/getResultsForSubCategory/{category}/{subcat}", "ResultsController@getResultsForSubCategoryForView");
+Route::get("/getResultsByCategoryForFront", "ResultsController@getResultsByCategoryForFront");
 
 
 // ------------------------------------
 // базовые контроллеры
 // ------------------------------------
-Route::get('/', 				'IndexController@init');
-Route::post('create', 			'AdvertController@createAdvert');
-Route::get('home',	 		 	'CabinetController@index');
-Route::get('home/{advert_id}',	'CabinetController@index');
-Route::get('podat-obyavlenie', 	'AdvertController@newAdvert');
-Route::get('getRegions', 		'IndexController@getRegions');
-Route::get('getPlaces', 		'IndexController@getPlaces');
-Route::get('getUser', 			'UserController@getUser');
-Route::get('categories', 		'CategoriesController@index');
+Route::get("/", 				"IndexController@init");
+Route::post("create", 			"AdvertController@createAdvert");
+Route::get("home",	 		 	"CabinetController@index");
+Route::get("home/{advert_id}",	"CabinetController@index");
+Route::get("podat-obyavlenie", 	"AdvertController@newAdvert");
+Route::get("getRegions", 		"IndexController@getRegions");
+Route::get("getPlaces", 		"IndexController@getPlaces");
+Route::get("getUser", 			"UserController@getUser");
+Route::get("categories", 		"CategoriesController@index");
 
-Route::get('getSubCats',  'SubCatsController@getSubCats' );
+Route::get("getSubCats",  "SubCatsController@getSubCats" );
 
 // ---------------------------------------------------------------
 // авто
 // ---------------------------------------------------------------
-Route::get('getCarsMarks', 'AdvertController@getCarsMarks' );
-Route::get('getCarsModels', 'AdvertController@getCarsModels' );
-Route::get('search',  function () { return view('search')->with("items", "123"); });
+Route::get("getCarsMarks", "AdvertController@getCarsMarks" );
+Route::get("getCarsModels", "AdvertController@getCarsModels" );
+Route::get("search",  function () { return view("search")->with("items", "123"); });
 
 // ----------------------------------------
 // перенести в контроллер Categories
 // ----------------------------------------
 Route::get("/category/{id}", function ($id) {
 
-	$categories = DB::table('categories')->where("id", $id)->get();
+	$categories = DB::table("categories")->where("id", $id)->get();
 	$items = DB::table("adverts")->where("category_id", $id)->get();
 	return view("results")->with("items", $items )->with("category_id", $id )->with("category_name", mb_strtolower($categories[0]->name));
 
@@ -118,17 +118,17 @@ Route::get("logout", "\App\Http\Controllers\Auth\LoginController@logout");
 // ------------------------------------
 // категории по региону
 // ------------------------------------
-Route::get('{region}/{category}', 'ResultsController@getResultsByRegion');
+Route::get("{region}/{category}", "ResultsController@getResultsByRegion");
 
 // ------------------------------------
 // категории по региону и местности
 // ------------------------------------
-Route::get('{region}/{place}/{category}', 'ResultsController@getResultsByPlace');
+Route::get("{region}/{place}/{category}", "ResultsController@getResultsByPlace");
 
 /*
 ------------------------------------------------------------------------------------------------
  Панель администратора
 ------------------------------------------------------------------------------------------------*/
-Route::get('adminpanel', 'AdminController@login');
-Route::get('moderation',  function () { return view('moderation'); });
-Route::get('moderation/{advert_id}',  function () { return view('moderation_advert'); });
+Route::get("adminpanel", "AdminController@login");
+Route::get("moderation",  function () { return view("moderation"); });
+Route::get("moderation/{advert_id}",  function () { return view("moderation_advert"); });
