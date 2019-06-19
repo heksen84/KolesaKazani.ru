60ec283c72727ba4535147599ca4a95a39575a2e
Ilya Bobkov <ilya.bobkov@erg.kz>
2019-06-19 16:07:34 +0600

    -

--- resources/views/testSSR.blade.php
+++ resources/views/testSSR.blade.php
@@ -9,23 +9,23 @@
 <!-- Верхнее меню -->
 <div id="navbar_menu">
   <nav class="navbar navbar-dark bg-primary">
-  <a class="navbar-brand" href="#"><h1 style="font-size:30px">Дамеля</h1><h2 style="font-size:16px;margin-top:-5px;font-weight:500">все объявления Казахстана</h2></a>
+  <a class="navbar-brand" href="#"><h1 style="font-size:30px;font-weight:600">Дамеля</h1><h2 style="font-size:16px;margin-top:-5px;font-weight:500">все объявления Казахстана</h2></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Подать объявления <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/podat-obyavlenie">Подать объявления <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Мои объявления</a>
+        <a class="nav-link" href="/home">Мои объявления</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Вход</a>
+        <a class="nav-link" href="/login">Вход</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Регистрация</a>
+        <a class="nav-link" href="/register">Регистрация</a>
       </li>      
     </ul>
   </div>
@@ -43,15 +43,15 @@
   <div class="col-sm-12 col-md-12 col-lg-3 col-xl-3" style="text-align:center">      
     <div id="logo_block">
       <div id="logo_block_text">Дамеля</div>
-      <div style="font-size:16px;color:yellow;margin-top:-13px;letter-spacing:2px;">все объявления Казахстана</div>
+      <div style="font-size:16px;color:yellow;margin-top:-13px;letter-spacing:1px;">все объявления Казахстана</div>
     </div>
   </div>
 
  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-6" style="text-align:center">
-  <b-form>
+  <form>
     <input type="text" id="search_string" placeholder="поиск по объявлениям" required/>
     <button id="button_search" type="submit" title="Найти что требуется">Найти</button>
-   </b-form>    
+   </form>    
   </div>
 
     <div class="col-sm-12 col-md-12 col-lg-3 col-xl-3" style="text-align:center" title="Подать новое объявление" id="new_advert_col">
@@ -69,8 +69,9 @@
     </div>  
   </div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 </body>
 </html>
