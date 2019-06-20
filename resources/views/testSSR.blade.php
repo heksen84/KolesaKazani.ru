381327982f647047408cc9fd4f4d3c687946aebd
Ilya Bobkov <ilya.bobkov@erg.kz>
2019-06-20 15:20:09 +0600

    up+

--- resources/views/testSSR.blade.php
+++ resources/views/testSSR.blade.php
@@ -89,8 +89,19 @@
         @endforeach
       </div>
     </div>
-  </div>  
+  </div> 
+
+<!-- РЕКЛАМА -->
+<div class="row" style="margin-top:20px">
+  <h5 style="margin:auto">Google Advert</h5>
 </div>
+
+<div class="row" style="margin-top:40px">
+  <div id="footer"><a href="advertisers" class="underline_link">Реклама</a> | <a href="rules" class="underline_link">Правила сайта</a> | <a href="about" class="underline_link">О сайте</a></div>
+</div>
+
+</div>
+
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
