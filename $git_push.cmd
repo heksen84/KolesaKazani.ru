cbaec776561162dda72d5016eb256ea2739c1d40
Ilya Bobkov <ilya.bobkov@erg.kz>
2019-06-28 16:39:25 +0600

    up+

--- $git_push.cmd
+++ $git_push.cmd
@@ -4,4 +4,5 @@ git status
 git add .
 git commit -m "up+"
 REM git pull
+git config --global user.email "heksen84@yandex.ru"
 git push -u origin master
--- resources/views/testSSR.blade.php
+++ resources/views/testSSR.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
   <title>Доска объявлений Дамеля - все объявления Казахстана.</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 </head>
+
 <body>
 <div id="app">
 <div id="navbar_menu">
