9c273b305304a0dadc923c675927f8dc6cded239
heksen84 <heksen84@yandex.ru>
2019-06-28 16:44:12 +0600

    up+

--- resources/views/testSSR.blade.php
+++ resources/views/testSSR.blade.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="ru">
-
 <head>
   <title>Доска объявлений Дамеля - все объявления Казахстана.</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 </head>
-
 <body>
 <div id="app">
 <div id="navbar_menu">
@@ -16,7 +14,6 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
