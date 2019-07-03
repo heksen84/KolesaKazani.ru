831e2870774404d354dccdc8458ede1b7a25cf58
Ilya <heksen84@yandex.ru>
2019-07-03 19:50:47 +0600

    up+

--- resources/views/testResults.blade.php
+++ resources/views/testResults.blade.php
@@ -29,7 +29,7 @@
             <div class="card-body">              
               <h5 class="card-title">{{ $item["title"] }}</h5>
                 <p class="card-text">Описание</p>
-              <a href="#" class="btn btn-success">Подробнее</a>
+              <a href="#" class="btn btn-success btn-sm">Подробнее</a>
             </div>
         </div>
       </div>
