1395d9a9065955f4bc7310e7b60bf716f3f3a270
Ilya <heksen84@yandex.ru>
2019-07-28 13:01:52 +0600

    -

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -124,12 +124,13 @@
          <button style="border:1px solid white;font-size:14px" id="close_subcats_btn" class="btn-sm btn-primary hide" @click="returnToCategories">&#8634; Назад</button>
           <div id="subcategories" class="form-inline center">                                      
             @foreach($subcategories as $subcategory)       
-              <div class="col-sm-12 col-md-12 col-lg-12 col-xl-3 hide subitem" data-category-id="{{ $subcategory['category_id'] }}">
+              <div class="col-sm-12 col-md-12 col-lg-12 col-xl-3 hide" data-category-id="{{ $subcategory['category_id'] }}">
                 <a href="/{{ $subcategory['url'] }}" class="url"><div class="category_item subcategory">{{ $subcategory["name"] }}</div></a>
               </div>
             @endforeach
           </div>
         </div>
+        
       </div>
     </div>
   
