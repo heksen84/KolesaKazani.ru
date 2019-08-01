fcc0c9d2f34ebbfe2c7a4371b47326b99c2497e7
heksen84 <heksen84@yandex.ru>
2019-08-01 14:59:32 +0600

    -

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -25,16 +25,20 @@ class IndexController extends Controller {
 			}
 
 			if ($region!=null && $place===null) {
-
-				$region = Regions::select("name")->where("url", $region)->get();	
 				
+				$region = Regions::select("name")->where("url", $region)->get();	
+
 				if ($region->count()>0) {
 					$petrovich = new Petrovich(Petrovich::GENDER_MALE);
-					$title = "Доска объявлений Дамеля, все объявления ".$petrovich->firstname($region[0]->name, Petrovich::CASE_PREPOSITIONAL);
+					
+					//$regionName = $region[0]->name;
+					//$regionName = trim(str_replace("обл.", "", $region[0]->name));
+					//$title = "Доска объявлений Дамеля, все объявления ".$petrovich->firstname($regionName, Petrovich::CASE_PREPOSITIONAL);
+					$title = "Доска объявлений Дамеля, объявления ".$region[0]->name;
 					$description = "Описание";
 				}
 				else 
-					return view("errors/404");
+					return view("errors/404"); // редирект
 			}
 
 			if ($region!=null && $place!=null) {				
@@ -43,11 +47,11 @@ class IndexController extends Controller {
 				
 				if ($place->count()>0) {
 					$petrovich = new Petrovich(Petrovich::GENDER_MALE);
-					$title = "Доска объявлений Дамеля, все объявления в ".$petrovich->firstname($place[0]->name, Petrovich::CASE_PREPOSITIONAL);;
+					$title = "Доска объявлений Дамеля, объявления ".$petrovich->firstname($place[0]->name, Petrovich::CASE_GENITIVE);
 					$description = "Описание";
 				}
 				else 
-					return view("errors/404");
+					return view("errors/404"); // редирект
 			}
 		
 			$subcats = DB::table("subcats")->join("categories", "categories.id", "=", "subcats.category_id")->select("subcats.*", "categories.url as category_url")->get();
