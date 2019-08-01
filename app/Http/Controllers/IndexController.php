3fb2fd97ae29be19f28970e0fea8e6d2f59b4fc7
heksen84 <heksen84@yandex.ru>
2019-08-01 14:36:15 +0600

    -

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\File;
 use DB;
 
+use App\Helpers\Petrovich;
 use App\Categories;
 use App\Regions;
 use App\Places;
@@ -17,25 +18,36 @@ class IndexController extends Controller {
 		    
     // Базовая функция для главной страницы
     private function ShowIndexPage($region, $place) {
-
-			\Debugbar::info("REGION: ".$region);	
-			\Debugbar::info("PLACE: ".$place);
-			
-			$title = ""; $description = "";	    
-			
+						
 			if ($region===null && $place===null) {
 				$title = "Доска объявлений Дамеля, все объявления Казахстана";
 				$description = "Описание";
 			}
 
 			if ($region!=null && $place===null) {
-				$title = "Доска объявлений Дамеля, все объявления --- области";
-				$description = "Описание";
+
+				$region = Regions::select("name")->where("url", $region)->get();	
+				
+				if ($region->count()>0) {
+					$petrovich = new Petrovich(Petrovich::GENDER_MALE);
+					$title = "Доска объявлений Дамеля, все объявления ".$petrovich->firstname($region[0]->name, Petrovich::CASE_PREPOSITIONAL);
+					$description = "Описание";
+				}
+				else 
+					return view("errors/404");
 			}
 
-			if ($region!=null && $place!=null) {
-				$title = "Доска объявлений Дамеля, все объявления ---";
-				$description = "Описание";
+			if ($region!=null && $place!=null) {				
+				
+				$place = Places::select("name")->where("url", $place)->get();
+				
+				if ($place->count()>0) {
+					$petrovich = new Petrovich(Petrovich::GENDER_MALE);
+					$title = "Доска объявлений Дамеля, все объявления в ".$petrovich->firstname($place[0]->name, Petrovich::CASE_PREPOSITIONAL);;
+					$description = "Описание";
+				}
+				else 
+					return view("errors/404");
 			}
 		
 			$subcats = DB::table("subcats")->join("categories", "categories.id", "=", "subcats.category_id")->select("subcats.*", "categories.url as category_url")->get();
