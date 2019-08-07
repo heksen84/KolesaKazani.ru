b16a07e94226fb30c899705587c3375e636af913
heksen84 <heksen84@yandex.ru>
2019-08-07 12:29:11 +0600

    -

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -28,66 +28,70 @@ class IndexController extends Controller {
 	// ------------------------------------------------
     private function ShowIndexPage($region, $place) {			
 						
-			// Страна
-			if ($region===null && $place===null) {				
-				$location = "/";				
-				$title = "Доска объявлений Дамеля, все объявления Казахстана";
+		// Страна
+		if ($region===null && $place===null) {				
+			$location = "/";				
+			$title = "Доска объявлений Дамеля, все объявления Казахстана";
+			$description = "Описание";
+			$locationName = "Казахстан";
+		}
+
+		// Регион
+		if ($region!=null && $place===null) {
+
+			$location = $region;
+			$locationName = Regions::select("name")->where("url", $region)->get();
+			$locationName = $locationName[0]->name;
+			$regionArr = Regions::select("name")->where("url", $region)->get();	
+
+			if ($regionArr->count()>0) {
+				$petrovich = new Petrovich(Petrovich::GENDER_MALE);
+					
+				//$regionName = $region[0]->name;
+				//$regionName = trim(str_replace("обл.", "", $region[0]->name));
+				//$title = "Доска объявлений Дамеля, все объявления ".$petrovich->firstname($regionName, Petrovich::CASE_PREPOSITIONAL);
+
+				$title = "Доска объявлений Дамеля, объявления ".$regionArr[0]->name;
 				$description = "Описание";
 			}
-
-			// Регион
-			if ($region!=null && $place===null) {
-
-				$location = $region;
-
-				$regionArr = Regions::select("name")->where("url", $region)->get();	
-
-				if ($regionArr->count()>0) {
-					$petrovich = new Petrovich(Petrovich::GENDER_MALE);
-					
-					//$regionName = $region[0]->name;
-					//$regionName = trim(str_replace("обл.", "", $region[0]->name));
-					//$title = "Доска объявлений Дамеля, все объявления ".$petrovich->firstname($regionName, Petrovich::CASE_PREPOSITIONAL);
-
-					$title = "Доска объявлений Дамеля, объявления ".$regionArr[0]->name;
-					$description = "Описание";
-				}
 				else return view("errors/404"); // редирект
-			}
+		}
 
-			// Город, село
-			if ($region!=null && $place!=null) {
-				$location = $region."/".$place;
-				$placeArr = Places::select("name")->where("url", $place)->get();
+		// Город, село
+		if ($region!=null && $place!=null) {				
 				
-				if ($placeArr->count()>0) {
-					$petrovich = new Petrovich(Petrovich::GENDER_MALE);
-					$title = "Доска объявлений Дамеля, объявления ".$petrovich->firstname($placeArr[0]->name, Petrovich::CASE_GENITIVE);
-					$description = "Описание";
-				}
-				else return view("errors/404"); // редирект
+			$location = $region."/".$place;
+			$locationName = Places::select("name")->where("url", $place)->get();
+			$locationName = $locationName[0]->name;
+
+			$placeArr = Places::select("name")->where("url", $place)->get();
+				
+			if ($placeArr->count()>0) {
+				$petrovich = new Petrovich(Petrovich::GENDER_MALE);
+				$title = "Доска объявлений Дамеля, объявления ".$petrovich->firstname($placeArr[0]->name, Petrovich::CASE_GENITIVE);
+				$description = "Описание";
 			}
+			else return view("errors/404"); // редирект
+		}
 		
-			$subcats = DB::table("subcats")
-			->join("categories", "categories.id", "=", "subcats.category_id")
-			->select("subcats.*", "categories.url as category_url")
-			->get();
+		$subcats = DB::table("subcats")
+		->join("categories", "categories.id", "=", "subcats.category_id")
+		->select("subcats.*", "categories.url as category_url")
+		->get();
 
-			// Нужно вернуть категории уже с формированным url (например: pavlodarskaya-obl/aksu/transport/legkovoy-avtomobil)
-
-			\Debugbar::info("REGION: ".$region);
-			\Debugbar::info("PLACE: ".$place);
-									
-		
-			return view("index")
-			->with("locationName", "---")
-			->with("location", $location)
-			->with("categories", Categories::all())
-			->with("subcategories", json_decode($subcats, true))
-			->with("regions", Regions::all())
-			->with("auth", Auth::user()?1:0)
-			->with("title", $title)
-			->with("description", $description);
+		\Debugbar::info("location: ".$locationName);
+		\Debugbar::info("REGION: ".$region);
+		\Debugbar::info("PLACE: ".$place);
+											
+		return view("index")
+		->with("locationName", $locationName)
+		->with("location", $location)
+		->with("categories", Categories::all())
+		->with("subcategories", json_decode($subcats, true))
+		->with("regions", Regions::all())
+		->with("auth", Auth::user()?1:0)
+		->with("title", $title)
+		->with("description", $description);
     }
 
     // Cтрана
