8198c6e2c32d84be1f641cb65f03eb92e1f1a73e
heksen84 <heksen84@yandex.ru>
2019-08-29 17:48:37 +0600

    -

--- resources/views/newad.blade.php
+++ resources/views/newad.blade.php
@@ -11,48 +11,7 @@
 </head>
 <body>
   <div id="app">
-   <div class="container-fluid mycontainer_adv">
-    <div class="row">
-     <div class="col-sm-12 col-md-12 col-lg-10 col-xl-10 create_advert_col">
-
-	    <div class="close_button" title="Закрыть страницу" style="font-weight:bold" @click="closeAndReturn">X</div>
-	      <h1 class="title_text" style="margin-top:12px">Новое объявление</h1>
-	      <hr>
-        
-        <div id="loading" style="margin-bottom:10px">Подготовка...</div>
-
-        <div class="form-group hide" style="width:260px">
-          <label for="categories">Категория товара или услуги:</label>
-            <select class="form-control" v-model="category" @change="changeCategory">            
-              <option v-bind:value="null">-- Выберите категорию --</option>
-              <option v-bind:value="item.id" v-for="item in {{ $categories }}">${item.name}</option>
-              </select>
-          </label>
-        </div>
-
-        <div v-if="category!=null" style="margin-bottom:10px" class="hide">
-        <label style="width:270px">Вид сделки:</label>            
-        <div class="form-check" style="width:260px">
-            <div v-for="(item,index) in {{ $dealtypes }}" :key="index">
-              <input class="form-check-input" :id="item.id" type="radio" name="inlineRadioOptions" v-bind:value="item.id" v-model="sdelka" @change="setDeal">
-              <label class="form-check-label" :for="item.id">${item.deal_name_1}</label>
-            </div>
-        </div>
-        </div>
-
-        <form id="advertform" @submit="onSubmit">
-
-          <!-- Категории -->
-			    <div v-if="root"></div>
-          <transport v-else-if="transport"/>
-          <!--<h1 v-else-if="transport" class="hide">transport</h1>
-          <h1 v-else-if="real_estate" class="hide">nedvizh</h1>-->
-        
-        </form>
-
-      </div>
-    </div>
-   </div>
+  	<newad></newad>
   </div>
 <script src="https://api-maps.yandex.ru/2.0-stable/?apikey=123&load=package.standard&lang=ru-RU" type="text/javascript"></script>
 <script type="text/javascript" src="{{ mix('js/newad.js') }}"></script>
