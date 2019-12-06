593126f2188d14af6e9eb2418d970ab64e4778ac
heksen84 <heksen84@yandex.ru>
2019-12-06 10:35:36 +0600

    -

--- app/Http/Controllers/TransportResultsController.php
+++ app/Http/Controllers/TransportResultsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Adverts;
 use App\SubCats;
 use App\CarMark;
 
@@ -17,15 +18,22 @@ class TransportResultsController extends Controller
     // -------------------------------------------------------------    
     public function getTransportResultsByCountryForView(Request $request, $subcategory) {
 
-       \Debugbar::info($subcategory);       
-
-       $subcategoryId = SubCats::select("id")->where("url_ru", $subcategory)->get();
-       \Debugbar::info($subcategoryId);
-
         // $filter = new TransportFilter($request);
         // $filter->getStartPage();
         // $filter->getStartPrice();
         // $filter->getEndPrice();        
+       
+
+       $subcategoryId = SubCats::select("id")->where("url_ru", $subcategory)->get();
+       
+       \Debugbar::info("субкатегория: ".$subcategory);       
+       \Debugbar::info("id субкатегории: ".$subcategoryId);      
+              
+        $items = DB::select("SELECT adv.id, adv.price, adv.text FROM `adverts` AS adv");
+
+        // adverts: categoryId, subCategoryId, subCategoryInnerId,
+        // where idSubcat = ...
+        
         
         // RU
        switch($subcategory) {
@@ -33,80 +41,15 @@ class TransportResultsController extends Controller
         // ЛЕГКОВОЕ АВТО
         case "legkovoy-avtomobil": {
 
-            $items = DB::select("SELECT                                    
-            adv.id, 
-            adv.price,
-            dealtype.deal_name_2, 
-            car_mark.name, 
-            car_model.name_rus,(
-                SELECT image FROM images WHERE advert_id = adv.id LIMIT 1) as imageName,
-                DATE_FORMAT(adv.created_at, '%d/%m/%Y в %H:%m') AS created_at
-                FROM `adverts` AS adv JOIN (dealtype, sub_transport, car_mark, car_model) 
-            ON (
-                adv.deal = dealtype.id AND 
-                adv.sub_category_id=sub_transport.id AND 
-                sub_transport.mark=car_mark.id_car_mark AND 
-                sub_transport.model = car_model.id_car_model
-                ) 
-            WHERE adv.category_id=1 AND sub_transport.type=0"
-            );
-                                
-            $total = count($items); // подсчитываю кол-во объявлений
-
-            \Debugbar::info("Число легковых тачек: ".$total);
-            \Debugbar::info($items);
-
-            $car_marks = DB::select("SELECT * FROM `car_mark` ORDER BY name");
-            
-            \Debugbar::info($car_marks);
-
             $title="Покупка, продажа, обмен, сдача в аренду легковых автомобилей в Казахстане";
             $description = "";
-            $keywords = "";
+            $keywords = "";            
 
-            return view("results")
-            ->with("title", $title)
-            ->with("description", $description)
-            ->with("keywords", $keywords)
-            ->with("items", $items)
-            ->with("itemsCount", $total)
-            ->with("car_marks", $car_marks)
-            ->with("subcategory", $subcategoryId);
-            
 	        break;
          }
 
          // ГРУЗОВОЕ АВТО
-         case "gruzovoy-avtomobil": {
-
-            $items = DB::select("SELECT                                    
-            adv.id, 
-            adv.price,
-            dealtype.deal_name_2, (
-                SELECT image FROM images WHERE advert_id = adv.id LIMIT 1) as imageName,
-                DATE_FORMAT(adv.created_at, '%d/%m/%Y в %H:%m') AS created_at
-                FROM `adverts` AS adv JOIN (dealtype, sub_transport) 
-            ON  (
-                adv.deal = dealtype.id AND 
-                adv.sub_category_id=sub_transport.id                
-                ) 
-            WHERE adv.category_id=1 AND sub_transport.type=1"
-            );
-                                
-            $total = count($items); // подсчитываю кол-во объявлений
-
-            $title="Покупка, продажа, обмен, сдача в аренду грузового авто в Казахстане";
-            $description = "";
-            $keywords = "";
-
-            return view("results")
-            ->with("title", $title)
-            ->with("description", $description)
-            ->with("keywords", $keywords)
-            ->with("items", $items)
-            ->with("itemsCount", $total)            
-            ->with("subcategory", $subcategoryId);
-
+         case "gruzovoy-avtomobil": {            
 	        break;
          }
 
@@ -136,9 +79,9 @@ class TransportResultsController extends Controller
 
 	    // --- kz ---
        }        
+
+       return view("results")->with("title", $title)->with("description", $description)->with("keywords", $keywords)->with("items", [])->with("itemsCount", 0);
 		                        
-
     }
-
 
 }
--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -18,92 +18,28 @@
   <div class="container-fluid container1">
     <hr>
       <h1 style="color:rgb(50,50,50)">{{ $title }}</h1>
-        <div style="color:rgb(50,50,50);text-decoration:underline">Найдено: {{ $itemsCount }}</div>
-          подкатегория: <b>{{ $subcategory[0]->id }}</b>          
-        <!--<form>
-        <br>
-            <div class="form-group">
-              <div class="row">                     
-              @if ($itemsCount>0)
-                @if ($subcategory[0]->id==1)
+        <div style="color:rgb(50,50,50);text-decoration:underline">Найдено: {{ $itemsCount }}</div>          
+        
 
-              <div class="col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3">                
-                  <label>Марка:</label>
-                  <select name="select" class="form-control">
-                    <option value="null">Выберите марку</option>              
-                    @foreach($car_marks as $item)          
-                      <option value={{ $item->id_car_mark }}>{{ $item->name }}</option>
-                    @endforeach
-                  </select>
-                </div>
-
-                <div class="col-10 col-sm-10 col-md-3 col-lg-3 col-xl-3">
-                  <label>Модель:</label>
-                    <select name="select" class="form-control">
-                      <option value="null">Выберите модель</option> 
-                    </select>
-                </div> 
-              
-                <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
-                  <label>Цена</label>
-                    <input type="text" size="10" required class="form-control" placeholder="от" v-model="priceFrom"></input> 
-                </div>
-
-                <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
-                  <label>-</label>
-                    <input type="text" size="10" required class="form-control" placeholder="до" v-model="priceTo"></input> 
-                </div>                            
-
-                <div class="col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2">
-                  <button type="button" class="btn btn-secondary form-control" @click="filter">применить</button>
-                </div>          
-
-              </div>
-                @endif
-              @endif            
-            </div>                                            
-          </form>-->       
-        <hr>
-
-    <div class="row">
-      @if ($itemsCount>0)
-        @foreach($items as $item)
-          <div class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
-            <div class="card text-left">
-              <!--<img src="..." class="card-img-top" alt="картинка">-->
-              <div class="card-body">          
-              @if ($subcategory[0]->id==1)
-                <h5 class="card-title">{{ $item->deal_name_2 }} {{  $item->name }} {{  $item->name_rus }}</h5>
-                <div style="font-size:12px;color:grey">Размещено {{ $item->created_at }}</div>
-                  <p class="card-text">Детали</p>
-                  <a href="#" class="btn btn-secondary btn-sm">Подробнее</a>
-                </div>
-              @endif
-            </div>
-          </div>
-          @endforeach
-        @endif
-      </div>   
-
-  <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-    <br>    
-    <ul class="pagination justify-content-center">
-      <li class="page-item disabled">
-        <span class="page-link">Назад</span>
-      </li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item active">
-        <span class="page-link">
-          2
+      <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <br>    
+        <ul class="pagination justify-content-center">
+        <li class="page-item disabled">
+          <span class="page-link">Назад</span>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item active">
+          <span class="page-link">
+            2
           <span class="sr-only">(current)</span>
         </span>
       </li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item">
-        <a class="page-link" href="#">Вперёд</a>
-      </li>
-    </ul>
-  </div>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Вперёд</a>
+          </li>
+        </ul>
+      </div>
 
   </div>  
 </div>
