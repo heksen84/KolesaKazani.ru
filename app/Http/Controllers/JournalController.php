e805854046ad8696fcf1957c1d10023537e1aff8
heksen84 <heksen84@yandex.ru>
2019-12-09 09:02:23 +0600

    up+

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -9,33 +9,24 @@ use App\Places;
 
 class JournalController extends Controller {
 
+   // Получить имена подкатегорий
     public function getSubCategoryNamesById(Request $request) {
-        \Debugbar::info("CATEGORY ID: ".$request->id);
         return SubCats::select( "id", "name_ru" )->where("category_id", $request->id)->get();
     }
 
-    /*
-    ----------------------------------
-    Выбор марок авто
-    ----------------------------------*/
+    // Выбор марок авто
     public function getCarsMarks() {
         $car_marks = CarMark::all("id_car_mark", "name");
         return $car_marks;
     }
         
-    /*
-    ----------------------------------------------------
-    Выбор моделей авто
-    ----------------------------------------------------*/
+    // Выбор моделей авто
     public function getCarsModels(Request $request) {
         return DB::table("car_model")->where("id_car_mark", $request->mark_id )->get();
     }
 
-    /*
-	---------------------------------------------
-	 Получить расположение
-	---------------------------------------------*/
-	public function GetPlaces(Request $request) {
+    // Получить расположение
+    public function GetPlaces(Request $request) {
         return Places::where("region_id",  $request->region_id )->orderBy("name", "asc")->get();
    }
     
