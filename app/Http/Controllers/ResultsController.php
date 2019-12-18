3bad7e6279792102d26bb1fbf8511d01e5405e91
heksen84 <heksen84@yandex.ru>
2019-12-18 15:59:39 +0600

    -

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -16,33 +16,50 @@ class ResultsController extends Controller {
     // $filter->getEndPrice();        
 
     // -------------------------------------------------------------
-    // Результаты по стране для вьюхи (results.blade.php)
+    // получить данные подкатегории
+    // -------------------------------------------------------------
+    private function getSubCategory(Request $request, $subcategory) {  
+        $table = new SubCats();         
+        return $table::select("id", "title", "description", "keywords")->where("url", $subcategory)->get();
+    }
+
+    // -------------------------------------------------------------
+    // получить расположение
+    // -------------------------------------------------------------
+    private function getLocation($lang) {  
+
+        $inLocation="";
+
+        switch($lang) {
+            case "kz": $inLocation = "в Казахстане"; break;
+            case "ru": $inLocation = "в России"; break;
+        }
+        return $inLocation;
+    }
+
+    // -------------------------------------------------------------
+    // получить хранилище изображений
+    // -------------------------------------------------------------
+    private function getImagePath() {      
+        return \Storage::disk('local')->url('app/images/preview/');
+    }
+
+
+    // -------------------------------------------------------------
+    // результаты по стране
     // -------------------------------------------------------------    
     public function getCountrySubCategoryResults(Request $request, $category, $subcategory) {  
        
-         \Debugbar::info("Язык: ".$request->lang);
+        \Debugbar::info("Язык: ".$request->lang);
+                       
+        $subcategories = $this->getSubCategory($request, $subcategory);                        
 
-         $inLocation=null;
-
-         switch($request->lang) {
-            case "kz": $inLocation = "в Казахстане"; break;
-            case "ru": $inLocation = "в России"; break;
-         }
-       
-        $table = new SubCats();
-
-        // получаю id подкатегории по названию в url
-        $subcategories = $table::select("id", "title", "description", "keywords")->where("url", $subcategory)->get();                                    
-                        
-	     $imagePath = \Storage::disk('local')->url('app/images/preview/');
-
-        $items = DB::select
-        (
+        $items = DB::select(
          "SELECT 
          adv.id, 
          adv.title,
          adv.price,
-         concat('".$imagePath."', (SELECT name FROM images WHERE images.advert_id=adv.id AND images.type=0 LIMIT 1)) AS imageName
+         concat('".$this->getImagePath()."', (SELECT name FROM images WHERE images.advert_id=adv.id AND images.type=0 LIMIT 1)) AS imageName
          FROM `adverts` AS adv WHERE subcategory_id = ".$subcategories[0]->id
          );            
    
@@ -51,11 +68,25 @@ class ResultsController extends Controller {
          \Debugbar::info($items);
                 
          return view("results")
-         ->with("title", $subcategories[0]->title." ".$inLocation)
+         ->with("title", $subcategories[0]->title." ".$this->getLocation($request->lang))
          ->with("description", $subcategories[0]->description)
          ->with("keywords", $subcategories[0]->keywords)
          ->with("items", $items)
-         ->with("itemsCount", count($items));	
-       	                        
+         ->with("itemsCount", count($items));       	                        
     }
+
+    // -------------------------------------------------------------
+    // результаты по региону
+    // -------------------------------------------------------------
+    public function getRegionSubCategoryResults(Request $request, $region, $category, $subcategory) {        
+        $subcategories = $this->getSubCategory($request, $subcategory);    
+    }
+
+    // -------------------------------------------------------------
+    // результаты по городу либо селу
+    // -------------------------------------------------------------
+    public function getCitySubCategoryResults(Request $request, $region, $city, $category, $subcategory) {
+        $subcategories = $this->getSubCategory($request, $subcategory);
+    }
+    
 }
