5e45d55891cbefd608662d0e40cf529e9e8cd1d5
heksen84 <heksen84@yandex.ru>
2019-12-19 09:59:43 +0600

    -

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\DB;
 use App\Adverts;
 use App\SubCats;
 use App\CarMark;
+use App\Regions;
+use App\Places;
 
 class ResultsController extends Controller {
 
@@ -18,20 +20,36 @@ class ResultsController extends Controller {
     // -------------------------------------------------------------
     // получить данные подкатегории
     // -------------------------------------------------------------
-    private function getSubCategoryData(Request $request, $subcategoryUrl) {  
+    private function getSubCategoryData(Request $request, $subcategory) {  
         $table = new SubCats();         
-        return $table::select("id", "title", "description", "keywords")->where("url", $subcategoryUrl)->get();
+        return $table::select("id", "title", "description", "keywords")->where("url", $subcategory)->get();
+    }
+
+    // -------------------------------------------------------------
+    // получить данные региона
+    // -------------------------------------------------------------
+    private function getRegionData($region) {                
+        $regionId = Regions::select("region_id", "name")->where("url", $region)->get();        
+        \Debugbar::info("ID региона: ".$regionId[0]->region_id);
+        return $regionId[0];
+    }
+
+    // -------------------------------------------------------------
+    // получить данные города / села
+    // -------------------------------------------------------------
+    private function getCityData($city) {                
+        $cityId = Places::select("city_id", "name")->where("url", $city)->get();        
+        \Debugbar::info("ID города/села: ".$cityId[0]->city_id);
+        return $cityId[0];
     }
 
     // -------------------------------------------------------------
     // получить расположение
     // -------------------------------------------------------------
-    private function getLocationName($country) {
+    private function getLocationName($country) {        
         
-        \Debugbar::info("Язык: ".$country);
-
-        $str="";
-
+        \Debugbar::info("Язык: ".$country);        
+        
         switch($country) {
             case "kz": $str = "в Казахстане"; break;
             case "ru": $str = "в России"; break;
@@ -57,7 +75,7 @@ class ResultsController extends Controller {
          "SELECT 
          adv.id, 
          adv.title,
-         adv.price,
+         adv.price,         
          concat('".$this->getImagePath()."', (SELECT name FROM images WHERE images.advert_id=adv.id AND images.type=0 LIMIT 1)) AS imageName
          FROM `adverts` AS adv WHERE subcategory_id = ".$subcategories[0]->id);            
    
@@ -78,7 +96,8 @@ class ResultsController extends Controller {
     // -------------------------------------------------------------
     public function getRegionSubCategoryResults(Request $request, $region, $category, $subcategory) {        
         
-        $subcategories = $this->getSubCategoryData($request, $subcategory);                        
+        $subcategories = $this->getSubCategoryData($request, $subcategory);
+        $regionData = $this->getRegionData($region);
 
         $items = DB::select(
          "SELECT 
@@ -86,14 +105,16 @@ class ResultsController extends Controller {
          adv.title,
          adv.price,
          concat('".$this->getImagePath()."', (SELECT name FROM images WHERE images.advert_id=adv.id AND images.type=0 LIMIT 1)) AS imageName
-         FROM `adverts` AS adv WHERE subcategory_id = ".$subcategories[0]->id);            
+         FROM `adverts` AS adv WHERE subcategory_id = ".$subcategories[0]->id." AND region_id = ".$regionData->region_id);            
    
          \Debugbar::info("субкатегория: ".$subcategory);       
          \Debugbar::info("id субкатегории: ".$subcategories);      
          \Debugbar::info($items);
+
+         $locationName = " в ".$regionData->name;
                 
          return view("results")
-         ->with("title", $subcategories[0]->title." ".$this->getLocationName($request->country))
+         ->with("title", $subcategories[0]->title." ".$locationName)
          ->with("description", $subcategories[0]->description)
          ->with("keywords", $subcategories[0]->keywords)
          ->with("items", $items)
@@ -105,7 +126,10 @@ class ResultsController extends Controller {
     // -------------------------------------------------------------
     public function getCitySubCategoryResults(Request $request, $region, $city, $category, $subcategory) {
 
-        $subcategories = $this->getSubCategoryData($request, $subcategory);                        
+        $subcategories = $this->getSubCategoryData($request, $subcategory);
+
+        $regionData = $this->getRegionData($region);
+        $cityData = $this->getCityData($city);                        
 
         $items = DB::select(
          "SELECT 
@@ -113,14 +137,17 @@ class ResultsController extends Controller {
          adv.title,
          adv.price,
          concat('".$this->getImagePath()."', (SELECT name FROM images WHERE images.advert_id=adv.id AND images.type=0 LIMIT 1)) AS imageName
-         FROM `adverts` AS adv WHERE subcategory_id = ".$subcategories[0]->id);            
+         FROM `adverts` AS adv WHERE subcategory_id = ".$subcategories[0]->id.
+         " AND city_id = ".$cityData->city_id." AND region_id = ".$regionData->region_id);
    
          \Debugbar::info("субкатегория: ".$subcategory);       
          \Debugbar::info("id субкатегории: ".$subcategories);      
          \Debugbar::info($items);
+
+         $locationName = " в ".$cityData->name;
                 
          return view("results")
-         ->with("title", $subcategories[0]->title." ".$this->getLocationName($request->country))
+         ->with("title", $subcategories[0]->title." ".$locationName)
          ->with("description", $subcategories[0]->description)
          ->with("keywords", $subcategories[0]->keywords)
          ->with("items", $items)
