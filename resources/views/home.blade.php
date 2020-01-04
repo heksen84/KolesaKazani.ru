e1afbbefd52869bd4155eb43f0fe0665422dacdb
Ilya <heksen84@yandex.ru>
2020-01-04 19:04:51 +0600

    *

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
   <div id="app"></div>
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">подать объявление</a>
+  <a href="#">подать объявление</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
