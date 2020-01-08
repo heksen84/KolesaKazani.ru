e4e71ea4e67868de78774c8cfda432ad3c3d60ec
heksen84 <heksen84@yandex.ru>
2020-01-08 10:38:32 +0600

    +

--- resources/views/filters/legkovoy.blade.php
+++ resources/views/filters/legkovoy.blade.php
@@ -1,1 +1,17 @@
-Legkovoy
+{{ $category }}
+<form class="form-inline" @submit="onSubmit">  		
+	<div class="form-group mx-1">
+		Цена:
+	</div>
+	<div class="form-group mx-1">
+		<label for="startPrice" class="sr-only">От</label>
+		<input type="text" id="startPrice" v-model="start_price" class="form-control form-control-sm" style="width:100px" placeholder="от" required/>
+	</div>
+	 <div class="form-group mx-1">
+		 <label for="endPrice" class="sr-only">До</label>
+		<input type="text" id="endPrice" v-model="end_price" class="form-control form-control-sm" style="width:100px" placeholder="до" required/>
+	</div>
+	<div class="form-group mx-1">
+		<button type="submit" class="btn btn-sm btn-warning">применить</button>
+	</div>
+</form>
