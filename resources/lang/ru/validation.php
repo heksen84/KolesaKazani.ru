2ad0fdbf00a4048dbac5e453b945e3475a453852
heksen84 <heksen84@yandex.ru>
2020-01-26 14:32:28 +0600

    up+

--- resources/lang/ru/validation.php
+++ resources/lang/ru/validation.php
@@ -61,7 +61,7 @@ return [
     'min'                  => [
         'numeric' => 'The :attribute must be at least :min.',
         'file'    => 'The :attribute must be at least :min kilobytes.',
-        'string'  => 'The :attribute must be at least :min characters.',
+        'string'  => ':attribute должен быть не менее :min символов.',
         'array'   => 'The :attribute must have at least :min items.',
     ],
     'not_in'               => 'The selected :attribute is invalid.',
@@ -116,6 +116,8 @@ return [
     |
     */
 
-    'attributes' => [],
+    'attributes' => [
+      'password'             => 'Пароль'
+    ],
 
 ];
