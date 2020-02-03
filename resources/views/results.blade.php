acb239710178cef630aa59bbf1f4457d90065487
heksen84 <heksen84@yandex.ru>
2020-02-03 17:08:26 +0600

    -

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -29,30 +29,26 @@
       <h1 class="grey">{{ $title }}</h1>
         <!--<div class="grey">Найдено: ({{ $items->count() }} из {{ $items->total() }} ) [ категория: {{ $categoryId }} подкатегория: {{ $subcategoryId }} ]</div>-->	        
           <div class="row" style="margin-top:35px">
-            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" style="margin-bottom:10px">
-              
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12" style="margin-bottom:10px">              
                 <!-- ФИЛЬТРЫ -->
                 @if ($categoryId===1 && $subcategoryId===1)  
                   @include('filters/transport/legkovoy')
                 @else
                   @include('filters/base')
-                @endif
-             
-              </div>              
-
-
-                @foreach($items as $item)
-                  <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">                  
-                    <a href="/objavlenie/{{ $item->id }}">
-                      <div class="card text-left" style="height:270px;margin:3px">
-                        <img class="card-img-top" src="{{ $item->imageName }}" alt="{{ $item->title }}">
-                          <div class="card-body">                                                                                 
-                            {{ $item->title }}
-                            <p class="card-text">{{ $item->price }} тнг.</p>
-                          </div>              
-                      </div>
-                    </a>
-                  </div>
+                @endif             
+            </div>
+            @foreach($items as $item)
+              <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">                  
+                <a href="/objavlenie/{{ $item->id }}">
+                  <div class="card text-left" style="height:270px;margin:3px">
+                    <img class="card-img-top" src="{{ $item->imageName }}" alt="{{ $item->title }}">
+                      <div class="card-body">                                                                                 
+                        {{ $item->title }}
+                        <p class="card-text">{{ $item->price }} тнг.</p>
+                      </div>              
+                    </div>
+                  </a>
+                </div>
                 @endforeach
 
                 <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">                
