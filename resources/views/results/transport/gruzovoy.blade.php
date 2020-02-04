4558fd9fa99ca3dfd796c3b9f16ce6c1ac104c71
heksen84 <heksen84@yandex.ru>
2020-02-04 16:51:20 +0600

    -

--- resources/views/results/transport/gruzovoy.blade.php
+++ resources/views/results/transport/gruzovoy.blade.php
@@ -1,5 +1,17 @@
-<p>Год выпуска: <b>{{ $advert->year }} г.</b></p>
-<p>Положение руля: <b>{{ $advert->steering_position }}</b></p>
-<p>Пробег: <b>{{ $advert->mileage }} км.</b></p>
-<p>Тип двигателя: <b>{{ $advert->engine_type }}</b></p>
-<p>Растоможен: <b>{{ $advert->customs }}</b></p>
+<div class="row">
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+        <p>Год выпуска: <b>{{ $advert->year }} г.</b></p> 
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+        <p>Положение руля: <b>{{ $advert->steering_position }}</b></p>
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+        <p>Пробег: <b>{{ $advert->mileage }} км.</b></p>
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+        <p>Тип двигателя: <b>{{ $advert->engine_type }}</b></p>
+    </div>
+    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+        <p>Растоможен: <b>{{ $advert->customs }}</b></p>
+    </div>
+</div>
--- resources/views/results/transport/legkovoy.blade.php
+++ resources/views/results/transport/legkovoy.blade.php
@@ -1,16 +1,21 @@
 <div class="row">
- <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+ 
+<div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
   <p>Марка: <b>{{ $advert->car_name }}</b></p>
  </div>
+ 
  <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
   <p>Модель: <b>{{ $advert->car_model }}</b></p>
  </div>
+ 
  <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
    <p>Год выпуска: <b>{{ $advert->year }} г.</b></p>
  </div>
+ 
  <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
    <p>Положение руля: <b>{{ $advert->steering_position }}</b></p>
  </div>
+
  <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
    <p>Пробег: <b>{{ $advert->mileage }} км.</b></p>
  </div>
