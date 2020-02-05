9265e8dbc1fc276402a2ffc1e156dc3bc05a1673
heksen84 <heksen84@yandex.ru>
2020-02-05 09:33:09 +0600

    -

--- resources/views/results/nedvizhimost/common.blade.php
+++ resources/views/results/nedvizhimost/common.blade.php
@@ -1,17 +1,27 @@
 <div class="row">
-    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-        <p>Год выпуска: <b>{{ $advert->year }} г.</b></p> 
-    </div>
-    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-        <p>Положение руля: <b>{{ $advert->steering_position }}</b></p>
-    </div>
-    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-        <p>Пробег: <b>{{ $advert->mileage }} км.</b></p>
-    </div>
-    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-        <p>Тип двигателя: <b>{{ $advert->engine_type }}</b></p>
-    </div>
-    <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
-        <p>Растоможен: <b>{{ $advert->customs }}</b></p>
-    </div>
+ 
+<div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+  <p>Этаж: <b>{{ $advert->car_name }}</b></p>
+ </div>
+ 
+ <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+  <p>Кол-во этажей в доме: <b>{{ $advert->car_model }}</b></p>
+ </div>
+ 
+ <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+   <p>Кол-во комнат: <b>{{ $advert->year }} г.</b></p>
+ </div>
+ 
+ <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+   <p>Площадь: <b>{{ $advert->steering_position }}</b></p>
+ </div>
+
+ <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+   <p>Право собственности: <b>{{ $advert->mileage }} км.</b></p>
+ </div>
+
+ <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-4">
+  <p>Вид объекта: <b>{{ $advert->engine_type }}</b></p>
+ </div>
+
 </div>
