f4db49b24c197e2796e4df24348e4a76dc70cae9
heksen84 <heksen84@yandex.ru>
2020-02-12 22:21:54 +0600

    -

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -20,7 +20,7 @@
     <div class="row">
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
 <!--        {{ $advert->category_id }} / {{ $advert->subcategory_id }}-->
-        <b style="font-size:13px;color:green">{{ $advert->created_at }}</b>
+        <div style="font-size:12px;color:grey">{{ $advert->created_at }}</div>
         <div id="location">{{ $advert->region_name }}, {{ $advert->city_name }}</div>
 
               @if ($advert->title!="null") 
