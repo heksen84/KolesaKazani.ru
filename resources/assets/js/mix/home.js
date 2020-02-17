2e0a26b44a06449ac4cc655a55f1936efe3883ec
heksen84 <heksen84@yandex.ru>
2020-02-17 13:47:54 +0600

    up+

--- resources/assets/js/mix/home.js
+++ resources/assets/js/mix/home.js
@@ -6,9 +6,38 @@ import "bootstrap";
 $( document ).ready(function() {
     
     // оплата
-    $(".actions button").click(function(item) {
-        //alert($(this).index());
+    $(".actions button").click(function(item) {        
+        
+        let title_text="";
+        let price=0;
+
+        switch($(this).index()) {
+            case 0: {
+                title_text = "срочно, торг"; 
+                price = 100;
+                break;
+            }
+            case 1: { 
+                title_text = "продлить"; 
+                price = 200;
+                break;
+            }
+            case 2: { 
+                title_text = "поднять в топ"; 
+                price = 300;
+                break;
+            }
+            case 3: { 
+                title_text = "покрасить"; 
+                price = 400;
+                break;
+            }
+        }
+        
+        $(".modal-title").text(title_text);
+        $("#price").text(price);
         $("#payment_window").modal('show');
+        
     });
 
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,13 +19,13 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Оплата услуги</h5>
+        <h5 class="modal-title"></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <h2>Цена 100 тнг.</h2>
+        <h4>Цена <span id="price"></span> тнг.</h4>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-primary">Оплатить</button>
