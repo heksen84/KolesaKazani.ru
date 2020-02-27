88292516eab448dbba1291c45c7a58492f4515ac
heksen84 <heksen84@yandex.ru>
2020-02-27 08:12:54 +0600

    up+

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,16 +1,27 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="keywords" content="Страница не найдена"/>
-        <meta name="description" content="Страница не найдена">
-        <title>Страница не найдена</title>
-	<link rel="icon" href="{{ asset('public/shop.ico') }}">
-    </head>
+<!DOCTYPE html>
+<html>
+<head>
+<title>404 – страница не найдена</title>
+<meta charset="utf-8">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+<style type="text/css">
+html, body {width:100%;height:100%;overflow:hidden;margin:0px;padding:0px;font-family:'Open Sans',sans-serif;font-size:16px}
+body {background:url('/resources/assets/images/errors/404.jpg') center no-repeat #dcecf9}
+.content {width:100%;text-align:center;position:absolute;bottom:10%;left:0px;}
+.content a {display:inline-block;text-decoration:none}
+.content a:hover {opacity:0.7}
+.content a, .content a:hover {color:#000;}
+@media only screen and (max-width: 460px), screen and (max-height: 700px) {
+.content {position:static;}
+.content a {display:block;width:100%;height:100%;position:absolute;top:0px;left:0px;font-size:0px;opacity:0;}
+body {background-size:cover}
+}
+</style>
+</head>
 <body>
-	<h1 style="text-align:center;margin-top:30px;color:rgb(100,100,130);">Такой страницы нет</h1>
-    	<h3 style="text-align:center"><a href="/" style="color:rgb(100,100,100);text-decoration:none">вернуться на главную</a></h3>
+<div class="content">
+	<a href="/">Перейти к главной странице</a>
+</div>
 </body>
 </html>
