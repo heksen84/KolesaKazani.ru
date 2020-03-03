035338e16df8b4a3b1a8ac1f06682f9dbc35c22a
heksen84 <heksen84@yandex.ru>
2020-03-03 09:44:40 +0600

    -

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="payment_window_title"></h5>
+        <h4 class="modal-title" id="payment_window_title"></h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -40,7 +40,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Удаление объявления</h5>
+        <h4 class="modal-title">Удаление объявления</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
