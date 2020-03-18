a8fcc3f5ca2c150abf574038cf44a01a25e58943
Ilya <heksen84@yandex.ru>
2020-03-18 21:10:39 +0600

    up+

--- resources/views/filters/transport/legkovoy.blade.php
+++ resources/views/filters/transport/legkovoy.blade.php
@@ -1,7 +1,7 @@
 
 <form style="background:rgb(200,255,200)">  
 	<div class="row p-2">
-		<div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+		<div class="col-8 col-sm-8 col-md-6 col-lg-3 col-xl-3">
 		<div class="form-group mx-1">
     <label for="exampleFormControlSelect1">Марка</label>
     <select class="form-control form-control-sm" id="exampleFormControlSelect1">
@@ -14,7 +14,7 @@
   	</div> 
 		</div>
 
-		<div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
+		<div class="col-8 col-sm-8 col-md-6 col-lg-3 col-xl-3">
 		<div class="form-group mx-1">
     <label for="exampleFormControlSelect2">Модель</label>
     <select class="form-control form-control-sm" id="exampleFormControlSelect2">
@@ -27,12 +27,13 @@
   	</div> 
 		</div>
 
-		<div class="col-12 col-sm-12 col-md-6 col-lg-2 col-xl-2">
+		<div class="col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2">
 			<div class="form-group mx-1">
 		 		<label for="year">Год выпуска</label>
 				<input type="text" id="year" class="form-control form-control-sm" required/>
 			</div>
 		</div>
+		
 
 		<div class="col-12 col-sm-12 col-md-6 col-lg-2 col-xl-2">
 			<div class="form-group mx-1">
@@ -41,7 +42,7 @@
 			</div>
 		</div>
 
-		<div class="col-12 col-sm-12 col-md-6 col-lg-2 col-xl-2 mb-2">
+		<div class="col-12 col-sm-6 col-md-6 col-lg-2 col-xl-2 mb-2">
 			<div class="form-group">
 		 		<label for="year">Цена до</label>
 				<input type="text" id="year" class="form-control form-control-sm" required/>
