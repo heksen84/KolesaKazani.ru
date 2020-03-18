8d4b04d61a25cdd043b6a33b74b46e518aa47e2a
Ilya <heksen84@yandex.ru>
2020-03-18 11:52:27 +0600

    -

--- resources/views/filters/transport/legkovoy.blade.php
+++ resources/views/filters/transport/legkovoy.blade.php
@@ -1,29 +1,41 @@
-<form class="form-inline" @submit="onSubmit">  		
-	<!--<div class="form-group mx-1">
-		Цена:
-	</div>-->
+
+<form style="background:rgb(200,255,200)">  
+			
+	
+	<!-- сделать на чистом js -->
+	<div class="form-group mx-1" style="width:200px">
+    <label for="exampleFormControlSelect1">Марка</label>
+    <select class="form-control form-control-sm" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  	</div>
+
+	<div class="form-group mx-1" style="width:200px">
+    <label for="exampleFormControlSelect2">Модель</label>
+    <select class="form-control form-control-sm" id="exampleFormControlSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  	</div>
+
+	<div class="form-group mx-1">
+		 <label for="year" class="sr-only">Год выпуска</label>
+		<input type="text" id="endPrice" class="form-control form-control-sm" style="width:100px" placeholder="Год выпуска" required/>
+	</div>
 	<div class="form-group mx-1">
 		<label for="startPrice" class="sr-only">От</label>
-		<input type="text" id="startPrice" v-model="start_price" class="form-control form-control-sm" style="width:100px" placeholder="от" required/>
+		<input type="text" id="startPrice" class="form-control form-control-sm" style="width:100px" placeholder="от" required/>
 	</div>
 	 <div class="form-group mx-1">
 		 <label for="endPrice" class="sr-only">До</label>
-		<input type="text" id="endPrice" v-model="end_price" class="form-control form-control-sm" style="width:100px" placeholder="до" required/>
-	</div>
-
-	<div class="form-group mx-1">
-		 <label for="endPrice" class="sr-only">Марка</label>
-		<input type="text" id="endPrice" v-model="end_price" class="form-control form-control-sm" style="width:100px" placeholder="Марка" required/>
-	</div>
-
-	<div class="form-group mx-1">
-		 <label for="endPrice" class="sr-only">Модель</label>
-		<input type="text" id="endPrice" v-model="end_price" class="form-control form-control-sm" style="width:100px" placeholder="Модель" required/>
-	</div>
-
-	<div class="form-group mx-1">
-		 <label for="endPrice" class="sr-only">Модель</label>
-		<input type="text" id="endPrice" v-model="end_price" class="form-control form-control-sm" style="width:100px" placeholder="Год выпуска" required/>
+		<input type="text" id="endPrice" class="form-control form-control-sm" style="width:100px" placeholder="до" required/>
 	</div>
 
 	<div class="form-group mx-1">
