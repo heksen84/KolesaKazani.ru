6c64a50dc172020d17047c9a67d1f7f113b3a0c1
Ilya <heksen84@yandex.ru>
2020-03-18 20:19:25 +0600

    -

--- resources/views/filters/transport/legkovoy.blade.php
+++ resources/views/filters/transport/legkovoy.blade.php
@@ -1,7 +1,7 @@
 
 <form style="background:rgb(200,255,200)">  
 	<div class="row p-2">
-		<div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+		<div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
 		<div class="form-group mx-1">
     <label for="exampleFormControlSelect1">Марка</label>
     <select class="form-control form-control-sm" id="exampleFormControlSelect1">
@@ -14,7 +14,7 @@
   	</div> 
 		</div>
 
-		<div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+		<div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
 		<div class="form-group mx-1">
     <label for="exampleFormControlSelect2">Модель</label>
     <select class="form-control form-control-sm" id="exampleFormControlSelect2">
@@ -27,28 +27,28 @@
   	</div> 
 		</div>
 
-		<div class="col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
+		<div class="col-12 col-sm-12 col-md-6 col-lg-2 col-xl-2">
 			<div class="form-group mx-1">
 		 		<label for="year">Год выпуска</label>
 				<input type="text" id="year" class="form-control form-control-sm" required/>
 			</div>
 		</div>
 
-		<div class="col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
+		<div class="col-12 col-sm-12 col-md-6 col-lg-2 col-xl-2">
 			<div class="form-group mx-1">
 		 		<label for="year">Цена от</label>
 				<input type="text" id="year" class="form-control form-control-sm" required/>
 			</div>
 		</div>
 
-		<div class="col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2 mb-2">
+		<div class="col-12 col-sm-12 col-md-6 col-lg-2 col-xl-2 mb-2">
 			<div class="form-group">
 		 		<label for="year">Цена до</label>
 				<input type="text" id="year" class="form-control form-control-sm" required/>
 			</div>
 		</div>
 
-		<div class="col-12 col-sm-12 col-md-2 col-lg-12 col-xl-12 mb-12 text-center">
+		<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-12 text-center">
 			<div class="form-group">
 				<button type="submit" class="btn btn-sm btn-primary">применить</button>
 			</div>
