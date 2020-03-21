071d470ec0867ea7b89be158fee44a855ee70790
Ilya <heksen84@yandex.ru>
2020-03-21 14:06:42 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -86,7 +86,7 @@
 
       <!-- кнопки выбора региона и т.п.-->
       <div class="index_select_region_and_other_button_block">    
-        <button class="btn btn-link" data-toggle="modal" id="locationButton" style="margin-top:-10px" @click="showLocationWindow">расположение {{ $locationName }}</button>
+        <button class="btn btn-link" data-toggle="modal" id="locationButton" style="margin-top:-8px" @click="showLocationWindow">расположение {{ $locationName }}</button>
         </div>
       </div>
 
