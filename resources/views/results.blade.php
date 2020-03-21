32d613bc1b1f885f920a82af13afde0daf36b1d5
Ilya <heksen84@yandex.ru>
2020-03-21 14:38:58 +0600

    up+

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -32,7 +32,7 @@
         <div class="row mt-5">
             
           <!-- ФИЛЬТРЫ -->  
-          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3" id="filters">                
+          <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3" id="filters-row">                
             @if ($categoryId===1 && $subcategoryId===1)  
               @include('filters/transport/legkovoy')
             @else
