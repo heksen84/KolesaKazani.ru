7e157a71ef1458177f36fe9fe61c9d4c67bcc91b
Ilya <heksen84@yandex.ru>
2020-03-23 12:43:12 +0600

    up+

--- resources/views/filters/transport/legkovoy.blade.php
+++ resources/views/filters/transport/legkovoy.blade.php
@@ -5,7 +5,7 @@
 @else
 	<form id="filters-form" action="/c/{{$category}}/{{$subcategory}}">
 @endif
-	<div class="row p-3">
+	<div class="row p-1">
 		<div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
 			<div class="form-group mx-1">
     			<label for="mark">Марка</label>
@@ -27,24 +27,35 @@
   			</div> 
 		</div>
 
-		<div class="col-4 col-sm-4 col-md-4 col-lg-2 col-xl-2">
+		<div class="col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2">
 			<div class="form-group mx-1">
-		 		<label for="year">Год</label>
-				<input type="number" id="year" class="form-control form-control-sm" name="year" value="{{$year}}"/>
+		 		<label for="year">Год выпуска</label>
+				<input type="number" placeholder="от" id="year" class="form-control form-control-sm" name="year" value="{{$year}}"/>
 			</div>
 		</div>
 
-		<div class="col-4 col-sm-4 col-md-4 col-lg-2 col-xl-2">
+		<div class="col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2">
 			<div class="form-group mx-1">
-		 		<label for="mileage_ot">Пробег от</label>
-				<input type="number" placeholder="км." id="mileage_ot" class="form-control form-control-sm" name="year" value="{{$year}}"/>
+		 		<label for="year_do">&nbsp;</label>
+				<input type="number" placeholder="до" id="year_do" class="form-control form-control-sm" name="year_do" value="{{$year}}"/>
 			</div>
 		</div>
 
-		<div class="col-4 col-sm-4 col-md-4 col-lg-2 col-xl-2">
+		</div>
+
+		<div class="row p-1" style="margin-top:-15px">
+		
+		<div class="col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2">
 			<div class="form-group mx-1">
-		 		<label for="mileage_do">Пробег до</label>
-				<input type="number" placeholder="км." id="mileage_do" class="form-control form-control-sm" name="year" value="{{$year}}"/>
+		 		<label for="mileage_ot">Пробег (км.)</label>
+				<input type="number" placeholder="от" id="mileage_ot" class="form-control form-control-sm" name="year" value="{{$year}}"/>
+			</div>
+		</div>
+
+		<div class="col-5 col-sm-5 col-md-5 col-lg-2 col-xl-2">
+			<div class="form-group mx-1">
+		 		<label for="mileage_do">&nbsp;</label>
+				<input type="number" placeholder="до" id="mileage_do" class="form-control form-control-sm" name="year" value="{{$year}}"/>
 			</div>
 		</div>
 
@@ -58,17 +69,21 @@
   			</div> 
 		</div>
 
+		</div>
+
+		<div class="row p-1" style="margin-top:-15px">
+
 		<div class="col-4 col-sm-4 col-md-4 col-lg-2 col-xl-2">
 			<div class="form-group mx-1">
-		 		<label for="price_ot">Цена от</label>
-				<input type="number" id="price_ot" class="form-control form-control-sm" name="price_ot" value="{{$price_ot}}" />
+		 		<label for="price_ot">Цена</label>
+				<input type="number" placeholder="от" id="price_ot" class="form-control form-control-sm" name="price_ot" value="{{$price_ot}}" />
 			</div>
 		</div>
 
 		<div class="col-4 col-sm-4 col-md-4 col-lg-2 col-xl-2">
 			<div class="form-group mx-1">
-		 		<label for="price_do">Цена до</label>
-				<input type="number" id="price_do" class="form-control form-control-sm" name="price_do" value="{{$price_do}}" />
+		 		<label for="price_do">&nbsp;</label>
+				<input type="number" placeholder="до" id="price_do" class="form-control form-control-sm" name="price_do" value="{{$price_do}}" />
 			</div>
 		</div>
 
