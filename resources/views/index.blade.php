38f8c5b060e4381a46d8875a5400098865cf5be0
Ilya <heksen84@yandex.ru>
2020-03-26 10:00:44 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -144,7 +144,7 @@
     </div>
   @endif
 
-  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
+  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center pt-1">
   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- Главная страница (рекоменд.) -->
 <ins class="adsbygoogle"
