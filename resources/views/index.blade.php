beca60a7a1863000ebccdddd218d7fc90fcd29f3
Ilya <heksen84@yandex.ru>
2020-03-26 10:11:06 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -129,7 +129,7 @@
     </div>
 
 
-  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center mt-1">
+  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center mt-2">
   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
   <!-- Главная страница (рекоменд.) -->
   <ins class="adsbygoogle"
