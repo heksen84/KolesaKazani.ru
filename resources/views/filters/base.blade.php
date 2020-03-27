a0e1c99a2719deb61391a8facc57167d991cdbcd
Ilya <heksen84@yandex.ru>
2020-03-27 18:24:17 +0600

    up+

--- resources/views/filters/base.blade.php
+++ resources/views/filters/base.blade.php
@@ -21,12 +21,6 @@
 			</div>
 		</div>
 
-		<!--<div class="col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2 text-center">
-			<div class="form-group mx-1">
-				<button type="submit" class="btn btn-sm btn-success">применить</button>
-			</div>
-		</div>-->
-
 		<div class="col-12 col-sm-12 col-md-4 col-lg-2 col-xl-2">
 			<div class="form-group mx-1">
 		 		<label>&nbsp;</label>
