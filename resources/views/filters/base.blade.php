e0ec7005b3c34a6139e1520e5c9cf6b056b63952
heksen84 <heksen84@yandex.ru>
2020-03-28 11:42:12 +0600

    up+

--- resources/views/filters/base.blade.php
+++ resources/views/filters/base.blade.php
@@ -8,8 +8,8 @@
 	
 	<div class="m-2 form-row">
 		<label for="price_ot" class="m-1">Цена:</label>
-		<input type="number" placeholder="от" id="price_ot" class="form-control form-control-sm col-3 col-md-1 ml-1" name="price_ot" value="{{$price_ot}}" />	
-		<input type="number" placeholder="до" id="price_do" class="form-control form-control-sm col-3 col-md-1 ml-1" name="price_do" value="{{$price_do}}" />	
+		<input type="number" placeholder="от" id="price_ot" class="form-control form-control-sm col-3 col-md-1 ml-1" name="price_ot" value="{{$price_ot}}" required/>	
+		<input type="number" placeholder="до" id="price_do" class="form-control form-control-sm col-3 col-md-1 ml-1" name="price_do" value="{{$price_do}}" required/>	
 		<button type="submit" class="btn btn-sm btn-success ml-1">применить</button>						 
 	</div>
 
