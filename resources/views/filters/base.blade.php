6b8947acfc8807f2e3452e73ebc28ff7497486cf
heksen84 <heksen84@yandex.ru>
2020-03-28 11:24:11 +0600

    up+

--- resources/views/filters/base.blade.php
+++ resources/views/filters/base.blade.php
@@ -1,34 +1,16 @@
 @if ($region && !$city)
-	<form id="filters-form" action="/{{$region}}/c/{{$category}}/{{$subcategory}}">
+	<form class="form-inline" id="filters-form" action="/{{$region}}/c/{{$category}}/{{$subcategory}}">
 @elseif ($region && $city)
-	<form id="filters-form" action="/{{$region}}/{{$city}}/c/{{$category}}/{{$subcategory}}">
+	<form class="form-inline" id="filters-form" action="/{{$region}}/{{$city}}/c/{{$category}}/{{$subcategory}}">
 @else
-	<form id="filters-form" action="/c/{{$category}}/{{$subcategory}}">
-@endif
-		<div class="row p-2">
-
-		<div class="col-4 col-sm-4 col-md-4 col-lg-2 col-xl-2">
-			<div class="form-group mx-1">
-		 		<label for="price_ot">Цена</label>
-				<input type="number" placeholder="от" id="price_ot" class="form-control form-control-sm" name="price_ot" value="{{$price_ot}}" />
-			</div>
-		</div>
-
-		<div class="col-4 col-sm-4 col-md-4 col-lg-2 col-xl-2">
-			<div class="form-group mx-1">
-		 		<label for="price_do">&nbsp;</label>
-				<input type="number" placeholder="до" id="price_do" class="form-control form-control-sm" name="price_do" value="{{$price_do}}" />
-			</div>
-		</div>
-
-		<div class="col-12 col-sm-12 col-md-4 col-lg-2 col-xl-2">
-			<div class="form-group mx-1">
-		 		<label>&nbsp;</label>
-				<button type="submit" class="btn btn-sm btn-success form-control form-control-sm">применить</button>
-			</div>
-		</div>
-
+	<form class="form-inline" id="filters-form" action="/c/{{$category}}/{{$subcategory}}">
+@endif		
 	
-
+	<div class="m-1 form-row">
+		<label for="price_ot" class="form-group m-1">Цена</label>
+		<input type="number" placeholder="от" id="price_ot" class="form-control form-control-sm form-group col-3 col-md-3 m-1" name="price_ot" value="{{$price_ot}}" />	
+		<input type="number" placeholder="до" id="price_do" class="form-control form-control-sm form-group col-3 col-md-3 m-1" name="price_do" value="{{$price_do}}" />	
+		<button type="submit" class="btn btn-sm btn-success m-1 mx-1 mb-2">применить</button>						 
 	</div>
+
 </form>
