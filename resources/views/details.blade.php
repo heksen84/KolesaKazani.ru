5e71137152a42878c533c7f89fd15eaed2b7113e
Ilya <heksen84@yandex.ru>
2020-03-29 22:20:03 +0600

    up+

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -90,15 +90,11 @@
 
                   <div id="carousel" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
-    </div>
+  @foreach($images as $image)
+                      <div class="carousel-item">
+                        <img src='{{ $image->name }}' alt='{{ $image->name }}' class="d-block w-100 img-fluid" loading="lazy"></img>
+                      </div>
+                      @endforeach
   </div>
 </div>
 
