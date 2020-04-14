930857538f79adfbbc2c7af7fdf8bcf5d4d7a0da
Ilya <heksen84@yandex.ru>
2020-04-14 17:07:22 +0600

    up+

--- resources/views/newad.blade.php
+++ resources/views/newad.blade.php
@@ -15,7 +15,7 @@
 <body>  
 
   <div id="loader">
-    <div class="d-flex justify-content-center mt-1">
+    <div class="d-flex justify-content-center mt-2">
       <button class="btn btn-primary" type="button" disabled>
         <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
         Подготовка объявления. Пожалуйста подождите.
