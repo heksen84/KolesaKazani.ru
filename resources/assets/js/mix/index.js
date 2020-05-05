44060ef8d08b4fba18750caf664f45c7a3ce2235
heksen84 <heksen84@yandex.ru>
2020-05-05 09:24:12 +0600

    up+

--- resources/assets/js/mix/index.js
+++ resources/assets/js/mix/index.js
@@ -47,7 +47,7 @@ $( document ).ready(function() {
       type: "GET",
       data: {"_token": $('meta[name="csrf-token"]').attr('content'), "region_id": $(this).attr("id")},
       success: function (response) {        
-        $("#places").append('<div style="text-align:center;margin-bottom:5px"><a href="'+selectedRegionUrl+'" class="grey link" style="background:yellow;margin:auto;font-size:17px">Искать в регионе</a></div>');
+        $("#places").append('<div style="text-align:center;margin-bottom:5px"><a href="'+selectedRegionUrl+'" class="grey link" style="background:yellow;margin:auto">Искать в регионе</a></div>');
         $.each(response, function(index, item) {               
           $("#places").append("<div style='display:inline-block;padding:6px;border:1px solid rgb(200,200,200);margin:3px'><a href='"+selectedRegionUrl+"/"+item.url+"' class='grey link text-center place_link'>"+item.name+"</a></div>");
         });                        
