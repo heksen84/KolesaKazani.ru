52782d159763bd65188b61db9c041e7025aa43b8
Ilya <heksen84@yandex.ru>
2020-05-11 12:35:20 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -203,7 +203,7 @@
       @foreach($newAdverts as $advert)      
       <div class="col-sm-12 col-md-3 col-lg-3 col-xl-2 m-3">      
       <a href="objavlenie/show/{{ $advert->id }}" class="black">
-        <div class="card" style="width:220px;height:400px;margin:auto">
+        <div class="card" style="width:220px;height:365px;margin:auto">
           <img class="card-img-top" src="{{ $advert->imageName }}" onerror="this.onerror=null;this.src='/public/images/_nofoto.jpg';" loading="lazy">          
             <div style="color:rgb(0,0,0);padding:3px;height:110px;white-space:initial;overflow:hidden;text-overflow:ellipsis;letter-spacing:1px;font-size:14px">
               {{ $advert->title }}              
