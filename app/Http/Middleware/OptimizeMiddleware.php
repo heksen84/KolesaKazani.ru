cc0c591904af099f5afdefaa6d3516530e3ee260
Ilya <heksen84@yandex.ru>
2020-05-11 18:59:24 +0600

    up+

--- app/Http/Middleware/OptimizeMiddleware.php
+++ app/Http/Middleware/OptimizeMiddleware.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ *
+ * (c) Farshad Ghanbari <eng.ghanbari2025@gmail.com>
+ *
+ */
+
 namespace App\Http\Middleware;
 
 use Closure;
@@ -8,13 +14,38 @@ class OptimizeMiddleware
 {
     /**
      * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * (c) Farshad Ghanbari <eng.ghanbari2025@gmail.com>
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        $response = $next($request);
+        $buffer = $response->getContent();
+        if (strpos($buffer, '<pre>') !== false) {
+            $replace = array(
+//                '/<!--[^\[](.*?)[^\]]-->/s' => '',
+//                "/<\?php/" => '<?php ',
+                "/\r/" => '',
+                "/>\n</" => '><',
+                "/>\s+\n</" => '><',
+                "/>\n\s+</" => '><',
+            );
+        } else {
+            $replace = array(
+ //               '/<!--[^\[](.*?)[^\]]-->/s' => '',
+ //               "/<\?php/" => '<?php ',
+                "/\n([\S])/" => '$1',
+                "/\r/" => '',
+                "/\n/" => '',
+                "/\t/" => '',
+                "/ +/" => ' ',
+            );
+        }
+        $buffer = preg_replace(array_keys($replace), array_values($replace), $buffer);
+        $response->setContent($buffer);
+        ini_set('zlib.output_compression', 'On');
+        return $response;
     }
 }
