5238281ad8242421eb96fe1932743fdb38d54009
Ilya <heksen84@yandex.ru>
2020-05-12 11:32:06 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -117,9 +117,27 @@
      (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
 
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <ins class="adsbygoogle"
+     style="display:block;width:180px;height:600px"
+     data-ad-client="ca-pub-8074944108437227"
+     data-ad-slot="2249357572"></ins>
+    <script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
   </div>  
 
   <div style="float:right" class="index-side-advert mr-4 mt-2">
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <ins class="adsbygoogle"
+     style="display:block;width:180px;height:600px"
+     data-ad-client="ca-pub-8074944108437227"
+     data-ad-slot="2249357572"></ins>
+    <script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
   <ins class="adsbygoogle"
      style="display:block;width:180px;height:600px"
