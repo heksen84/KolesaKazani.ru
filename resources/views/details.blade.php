eec32b7b85a1abbb7f49d46b981ce37d4d001b75
Ilya <heksen84@yandex.ru>
2020-05-16 21:48:39 +0600

    up+

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -68,8 +68,7 @@
                   <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
-                  </a>
-                  
+                  </a>                  
                   <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="sr-only">Next</span>
