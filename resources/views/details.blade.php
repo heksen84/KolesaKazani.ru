851263e2ba10bad13a4ba7e9eafa54de666f0ea4
Ilya <heksen84@yandex.ru>
2020-05-16 22:23:20 +0600

    up+

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -65,16 +65,16 @@
                   </div>
 
                   @if (count($images)>1)
-                  <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                  </a>                  
-                  <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                  </a>
+                    <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>                  
+                    <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
                   @endif
-          </div>
+              </div>
             
             <br>
               
