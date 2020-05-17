52e76697b659323907f2582973b2d675442dff33
Ilya <heksen84@yandex.ru>
2020-05-17 11:05:21 +0600

    up+

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -124,9 +124,8 @@
       </div>
 
   <!-- РЕКЛАМА -->
-  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <!-- Главная страница (рекоменд.) -->
+  <!--<div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>  
   <ins class="adsbygoogle"
      style="display:inline-block;width:100%;height:100px"
      data-ad-client="ca-pub-8074944108437227"
@@ -134,7 +133,7 @@
     <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
-  </div>
+  </div>-->
 </div>
 
 <script>
