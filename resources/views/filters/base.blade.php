7bf87df60c47d076e124157b4599e0eca625809a
Ilya <heksen84@yandex.ru>
2020-05-20 17:14:49 +0600

    up+

--- resources/views/filters/base.blade.php
+++ resources/views/filters/base.blade.php
@@ -27,7 +27,7 @@
 
 	<div class="form-row">
 		<div class="form-group col-12 text-center">
-			<button type="submit" class="btn btn-sm btn-outline-secondary" id="btn-submit">применить</button>
+			<button type="submit" class="btn btn-sm btn-outline-secondary">применить</button>
 		</div>												
 	</div>												
 		
