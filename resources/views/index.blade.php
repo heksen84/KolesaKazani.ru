92e9f44b516e361df715ab1c8cd54ea03afe186d
Ilya <heksen84@yandex.ru>
2020-06-03 21:25:03 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -274,7 +274,9 @@
       <div class="col-sm-12 col-md-3 col-lg-3 col-xl-2 m-3">      
       <a href="objavlenie/show/{{ $advert->id }}" class="black">
         <div class="card green-background" style="width:220px;height:365px;margin:auto">
+        @if (false)
         <div class="label-torg">срочно, торг</div>
+        @endif
           <img class="card-img-top" src="{{ $advert->imageName }}" onerror="this.onerror=null;this.src='/public/images/_nofoto.jpg';" loading="lazy">          
             <div class="card-title-text">{{ $advert->title }}</div>
             <div class="card-location-text">{{ $advert->region_name }}<br>{{ $advert->city_name }}</div>
