b46fb22af8866bddec72ec6712f32fa3a4b5e61c
heksen84 <heksen84@yandex.ru>
2020-07-24 15:12:31 +0600

    up+

--- resources/assets/js/mix/home.js
+++ resources/assets/js/mix/home.js
@@ -2,60 +2,69 @@ require('./bootstrap');
 import $ from "jquery";
 import "bootstrap";
 
+/*@if ($item->top)
+                  <span class="badge badge-primary" title="В топе с {{ date('d.m.Y', strtotime($item->topStartDate)) }} по {{ date('d.m.Y', strtotime($item->topFinishDate)) }}">В топе</span>
+                @endif
+                @if ($item->srochno)
+                  <span class="badge badge-danger" title="Срочное с {{ date('d.m.Y', strtotime($item->srochnoStartDate)) }} по {{ date('d.m.Y', strtotime($item->srochnoFinishDate)) }}">Срочное</span>
+                @endif
+                @if ($item->color)                  
+                  <span class="badge badge-success" title="Выделено с {{ date('d.m.Y', strtotime($item->colorStartDate)) }} по {{ date('d.m.Y', strtotime($item->colorFinishDate)) }}">Выделено</span>
+                @endif
+                @if ($item->color || $item->srochno || $item->top)
+                  <hr style="margin-top:8px">
+                @endif*/
+
+function setExtendOptions() {
+
+    $(".color").off().click(function(item) {
+
+        let self = $(this);
+
+        $.ajax({
+            url: "/objavlenie/makeExtend/"+$(this).parent().data("id")+"/makePaint",
+            type: "POST",
+            data: {"_token": $('meta[name="csrf-token"]').attr('content')},
+            success: function (response) {                
+                self.hide();
+            }
+        });        
+    });
+
+    $(".srochno").off().click(function(item) {
+
+        let self = $(this);
+
+        $.ajax({
+            url: "/objavlenie/makeExtend/"+$(this).parent().data("id")+"/srochno_torg",
+            type: "POST",
+            data: {"_token": $('meta[name="csrf-token"]').attr('content')},
+            success: function (response) {                
+                self.hide();
+            }
+        });        
+    });
+
+    $(".prodlit").off().click(function(item) {        
+
+        let self = $(this);
+
+        $.ajax({
+            url: "/objavlenie/makeExtend/"+$(this).parent().data("id")+"/prodlit",
+            type: "POST",
+            data: {"_token": $('meta[name="csrf-token"]').attr('content')},
+            success: function (response) {                
+                self.hide();                    
+                self.parent().append('<button class="btn btn-outline-success btn-sm m-1 top">В топ</button>');
+                self.parent().append('<button class="btn btn-outline-secondary btn-sm m-1 color">Выделить</button>');
+                self.parent().append('<button class="btn btn-outline-danger btn-sm m-1 srochno">Срочно</button>');                
+                setExtendOptions();             
+            }
+        });        
+    });
+}
+
 // html загружен
 $( document ).ready(function() {
-
-    function setExtendOptions() {
-
-        $(".color").off().click(function(item) {
-
-            let self = $(this);
-    
-            $.ajax({
-                url: "/objavlenie/makeExtend/"+$(this).parent().data("id")+"/makePaint",
-                type: "POST",
-                data: {"_token": $('meta[name="csrf-token"]').attr('content')},
-                success: function (response) {                
-                    self.hide();
-                }
-            });        
-        });
-    
-        $(".srochno").off().click(function(item) {
-    
-            let self = $(this);
-    
-            $.ajax({
-                url: "/objavlenie/makeExtend/"+$(this).parent().data("id")+"/srochno_torg",
-                type: "POST",
-                data: {"_token": $('meta[name="csrf-token"]').attr('content')},
-                success: function (response) {                
-                    self.hide();
-                }
-            });        
-        });
-
-        $(".prodlit").off().click(function(item) {        
-
-            let self = $(this);
-    
-            $.ajax({
-                url: "/objavlenie/makeExtend/"+$(this).parent().data("id")+"/prodlit",
-                type: "POST",
-                data: {"_token": $('meta[name="csrf-token"]').attr('content')},
-                success: function (response) {                
-                    self.hide();                    
-                    self.parent().append('<button class="btn btn-outline-success btn-sm m-1 top">В топ</button>');
-                    self.parent().append('<button class="btn btn-outline-secondary btn-sm m-1 color">Выделить</button>');
-                    self.parent().append('<button class="btn btn-outline-danger btn-sm m-1 srochno">Срочно</button>');                
-                    setExtendOptions();             
-                }
-            });        
-        });
-
-    }
-        
-
-    setExtendOptions();
-    
+    setExtendOptions();    
 });
