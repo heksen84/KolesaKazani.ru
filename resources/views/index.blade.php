23e34d941f969372f6017258eb0ccd3ca4004c86
heksen84 <heksen84@yandex.ru>
2020-08-31 09:31:56 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -289,6 +289,10 @@
       @endforeach
   </div>
   @endif
+
+  <div class="text-center">
+    <a href="/podat-objavlenie" class="black" style="font-size:18px;letter-spacing:3px">подать объявление</a>
+  </div>
   
   @if (count($newAdverts)>0)
   <div class="row text-center">
@@ -315,11 +319,7 @@
       </div>      
       @endforeach
   </div>
-  @endif
-
-  <div class="text-center">
-    <a href="/podat-objavlenie" class="black" style="font-size:18px;letter-spacing:3px">подать объявление</a>
-  </div>
+  @endif  
 
 <!-- РЕКЛАМА -->
 <!--  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center mt-5">
