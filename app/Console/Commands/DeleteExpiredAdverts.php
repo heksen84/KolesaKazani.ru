952fa003bed39322f37928791d9c67d7771f92ed
heksen84 <heksen84@yandex.ru>
2020-09-04 23:42:36 +0600

    up+

--- app/Console/Commands/DeleteExpiredAdverts.php
+++ app/Console/Commands/DeleteExpiredAdverts.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use App\Adverts;
 use App\Images;
 use App\Urls;
@@ -50,23 +51,7 @@ class DeleteExpiredAdverts extends Command
      * @return mixed
      */
     public function handle() {
-
-        /*
-            1. грохнуть запись с adverts
-            2. грохнуть запись с подкатегории если это транспорт или недвижимость
-            3. грохнуть запись с urls
-            4. грохнуть запись с adex_color
-            5. грохнуть запись с adex_srochno
-            6. грохнуть запись с adex_top
-            7. грохнуть запись с images
-        */
-
-        // if (currentDate >= finishDate+30 days)
-
-        // finishDate +30 day и потом удалить
-        
-        //$items = DB::table("adverts as adv")->whereRaw("DATE(NOW()) >= DATE_ADD('adv.finishDate', INTERVAL 3 DAY)")->get();
-        //$items = DB::table("adverts as adv")->whereRaw("adv.finishDate + INTERVAL 30 DAY >= NOW()")->get();
+    
         //$items = DB::table("adverts as adv", "adex_top", '00')
         /*->leftJoin("sub_transport", "adv.inner_id" , "=" , "sub_transport.id" )
         ->leftJoin("sub_realestate", "adv.inner_id" , "=" , "sub_realestate.id" )
@@ -78,7 +63,25 @@ class DeleteExpiredAdverts extends Command
         //->whereRaw("adv.finishDate + INTERVAL -30 DAY >= NOW()")
         //->whereRaw("adex_srochno.advert_id = adv.id")
         //->whereRaw("adex_top.advert_id = adv.id")
-        //->delete();
+        //->delete();        
+
+        $images = DB::table("images")->join("adverts", "adverts.id", "=", "images.advert_id")->whereRaw("adverts.finishDate + INTERVAL 30 DAY >= NOW()")->get();        
+        $this->info($images);
+        
+        foreach( $images as $img ) {
+
+            $this->info("----------------------------------------------------------------------");          
+            
+            if (Storage::disk('s3')->delete("images/normal/".$img->name)) {
+                $this->info("images/normal/".$img->name." удалён!");                            
+            }
+            
+            if (Storage::disk('s3')->delete("images/small/".$img->name)) {
+                $this->info("images/small/".$img->name." удалён!");                            
+            }
+            $this->info("----------------------------------------------------------------------\n");
+            
+        }	
         
         $items = DB::delete("DELETE 
         adverts, 
@@ -101,20 +104,6 @@ class DeleteExpiredAdverts extends Command
         );
  
         $this->info("Удалено: ".$items." записей");
-        
 
-        /*
-
-        Adverts::truncate();
-        Transport::truncate();
-	    RealEstate::truncate();        
-        Urls::truncate();        
-        Images::truncate();
-        
-        adex_color::truncate();
-        adex_srochno::truncate();
-        adex_top::truncate();*/
-
-        // здесь логика
     }
 }
