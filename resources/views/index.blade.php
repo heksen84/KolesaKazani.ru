682785ff98ebdabf243ef56163168c99bf275d63
heksen84 <heksen84@yandex.ru>
2020-09-10 19:41:53 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -289,6 +289,18 @@
       @endforeach
   </div>
   @endif
+
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!-- Горизонтальное после новых объявлений -->
+  <ins class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-8074944108437227"
+      data-ad-slot="8746851039"
+      data-ad-format="auto"
+       data-full-width-responsive="true"></ins>
+  <script>
+       (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
   
   @if (count($newAdverts)>0)
   <div class="row text-center">
