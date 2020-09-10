0e57a4d7e54b6620aafbcbb2e91e891ce78637a9
heksen84 <heksen84@yandex.ru>
2020-09-10 21:40:29 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -297,7 +297,7 @@
       data-ad-client="ca-pub-8074944108437227"
       data-ad-slot="8746851039"
       data-ad-format="auto"
-       data-full-width-responsive="true"></ins>
+      data-full-width-responsive="true"></ins>
   <script>
        (adsbygoogle = window.adsbygoogle || []).push({});
   </script>
