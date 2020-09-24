1344349151748721e24681efa1f1a8ae350635a7
heksen84 <heksen84@yandex.ru>
2020-09-24 17:24:30 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}" />
   <meta name="description" content="{{ $description }}" />
   <meta name="keywords" content="{{ $keywords }}" />
+  <meta name="yandex-verification" content="a29b2b5b1af5e2da" />
 
   <!-- OpenGraph -->
   <meta property="og:type" content="website">
