50a1be179555f009807c5cdf2ba563d30564c3da
heksen84 <heksen84@yandex.ru>
2020-09-25 13:37:00 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -290,20 +290,6 @@
       @endforeach
   </div>
   @endif
-
-  <!-- Горизонтальное до новых объявлений -->
-  <!-- data-ad-format="auto" -->
-  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center mt-1 mb-1">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>  
-    <ins class="adsbygoogle"
-        style="display:inline-block;width:100%;height:130px"
-        data-ad-client="ca-pub-8074944108437227"
-        data-ad-slot="8746851039"        
-        data-full-width-responsive="true"></ins>
-    <script>
-         (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-  </div>
   
   @if (count($newAdverts)>0)
   <div class="row text-center">
@@ -333,6 +319,19 @@
       @endforeach
   </div>
   @endif
+
+    <!-- data-ad-format="auto" -->
+    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center mt-1 mb-1">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>  
+    <ins class="adsbygoogle"
+        style="display:inline-block;width:100%;height:100px"
+        data-ad-client="ca-pub-8074944108437227"
+        data-ad-slot="8746851039"        
+        data-full-width-responsive="true"></ins>
+    <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+  </div>
 
   <div class="text-center mt-2">
     <a href="/podat-objavlenie" class="black" style="font-size:18px;letter-spacing:3px">подать объявление</a>
