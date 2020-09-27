a51ca9e7375d7f2fcbf5d06e6edba21aeafa70d6
heksen84 <heksen84@yandex.ru>
2020-09-27 15:25:20 +0600

    up+

--- app/Console/Commands/testSelectStorage.php
+++ app/Console/Commands/testSelectStorage.php
@@ -37,6 +37,15 @@ class testSelectStorage extends Command
      * @return mixed
      */
     public function handle() {
+
+	/*CASE
+	    WHEN condition1 THEN result1
+	    WHEN condition2 THEN result2
+	    WHEN conditionN THEN resultN
+	    ELSE result
+	END;*/
+
+	// вставить case в выборке
 	// WITH images AS (select id, title AS name, (SELECT 1) as imgStorageId from `adverts`) SELECT id, concat(name, ' /public/storage/', imgStorageId) AS path FROM images WHERE id = 1
     }
 }
