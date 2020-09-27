8e6de95bfa43f4266f16b8c51b323cc3c6ba3817
heksen84 <heksen84@yandex.ru>
2020-09-27 12:15:13 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,14 +35,14 @@
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(62690128, "init", {
+   ym(67694167, "init", {
         clickmap:true,
         trackLinks:true,
         accurateTrackBounce:true,
         webvisor:true
    });
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/62690128" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/67694167" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 
 <div id="app">
