28602bfd484a9b9c7e84f68e26c2fa32c36e5e08
heksen84 <heksen84@yandex.ru>
2020-10-01 13:22:27 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -334,7 +334,7 @@
     </div>
 
   <div class="text-center m-4 ">
-    <a href="/podat-objavlenie" class="black" style="font-size:23px;letter-spacing:3px;font-weight:300">подать объявление</a>
+    <a href="/podat-objavlenie" class="black" style="font-size:20px;letter-spacing:3px;font-weight:300">подать объявление</a>
   </div>
 
 <!-- РЕКЛАМА -->
