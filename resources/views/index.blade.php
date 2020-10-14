5c02847f52436f6b6759df45b8f9230e5b54c510
heksen84 <heksen84@yandex.ru>
2020-10-14 10:39:23 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}" />
   <meta name="description" content="{{ $description }}" />
   <meta name="keywords" content="{{ $keywords }}" />
-  <meta name="yandex-verification" content="a29b2b5b1af5e2da" />
+  <meta name="yandex-verification" content="56091dbfb213e164" />
 
   <!-- OpenGraph -->
   <meta property="og:type" content="website">
