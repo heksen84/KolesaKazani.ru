61fd4a3048e064eec5fa7a0e9c940bae9f4e5bfa
heksen84 <heksen84@yandex.ru>
2020-10-16 19:32:22 +0600

    up+

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -19,7 +19,7 @@ class Test extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Тест общего назначения. Для обкатки всякой ерунды.';
 
     /**
      * Create a new command instance.
