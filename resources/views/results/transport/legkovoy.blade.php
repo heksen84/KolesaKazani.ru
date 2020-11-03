4c5e73cff2109aa8a4dafca522142319cf9826b5
heksen84 <heksen84@yandex.ru>
2020-11-03 23:38:24 +0600

    up+

--- resources/views/results/transport/legkovoy.blade.php
+++ resources/views/results/transport/legkovoy.blade.php
@@ -1,35 +1,35 @@
 <div class="row">
  
+<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12"><hr></div>
+
 <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4">
-  <p>Марка: <span style="font-weight:300">{{ $advert->car_name }}</b></p>
+  <p>Марка: <span style="font-weight:300">{{ $advert->car_name }}</span></p>
  </div>
  
  <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4">
-  <p>Модель: <span style="font-weight:300">{{ $advert->car_model }}</b></p>
+  <p>Модель: <span style="font-weight:300">{{ $advert->car_model }}</span></p>
  </div>
  
  <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4">
-   <p>Год выпуска: <span style="font-weight:300">{{ $advert->year }} г.</b></p>
+   <p>Год выпуска: <span style="font-weight:300">{{ $advert->year }} г.</span></p>
  </div>
  
  <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4">
-   <p>Руль: <span style="font-weight:300">{{ $advert->steering_position }}</b></p>
+   <p>Руль: <span style="font-weight:300">{{ $advert->steering_position }}</span></p>
  </div>
 
  <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4">
-   <p>Пробег: <span style="font-weight:300">{{ $advert->mileage }} км.</b></p>
+   <p>Пробег: <span style="font-weight:300">{{ $advert->mileage }} км.</span></p>
  </div>
 
  <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4">
-  <p>Тип двигателя: <span style="font-weight:300">{{ $advert->engine_type }}</b></p>
+  <p>Тип двигателя: <span style="font-weight:300">{{ $advert->engine_type }}</span></p>
  </div>
 
  <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4">
-  <p>Растоможен: <span style="font-weight:300">{{ $advert->customs }}</b></p>
+  <p>Растоможен: <span style="font-weight:300">{{ $advert->customs }}</span></p>
  </div>
 
- <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-  <hr>
- </div>
+ <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12"><hr></div>
 
 </div>
