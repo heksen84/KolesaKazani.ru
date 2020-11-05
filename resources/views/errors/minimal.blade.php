927869242f2eba465b0ba764a3cb9caaf8d47078
heksen84 <heksen84@yandex.ru>
2020-11-05 17:37:14 +0600

    up+

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -63,6 +63,7 @@
 	        </div>                    
 	    </div>
 
+        <div class="text-center;margin-top:-20px">
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>  
                 <ins class="adsbygoogle"
                 style="display:block"
@@ -73,6 +74,7 @@
             <script>
             (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
+        </div>
 
     </body>
 </html>
