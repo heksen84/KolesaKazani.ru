b37d6bc719a067464c9bd73f5c5e0e5330d1d3fd
heksen84 <heksen84@yandex.ru>
2020-11-05 17:33:12 +0600

    up+

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -49,7 +49,7 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="flex-center position-ref full-width">
             <div class="code">
                 @yield('code')
             </div>
@@ -61,20 +61,18 @@
 	        <div style="padding: 10px">
 		        <a href="/">перейти на главную страницу</a>
 	        </div>        
+
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>  
+                <ins class="adsbygoogle"
+                style="display:block"
+                data-ad-client="ca-pub-8074944108437227"
+                data-ad-format="auto"
+                data-ad-slot="8746851039"        
+                data-full-width-responsive="true"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+
 	    </div>
-
-        ---
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>  
-        <ins class="adsbygoogle"
-          style="display:block"
-          data-ad-client="ca-pub-8074944108437227"
-          data-ad-format="auto"
-          data-ad-slot="8746851039"        
-          data-full-width-responsive="true"></ins>
-        <script>
-           (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-        ---
-
     </body>
 </html>
