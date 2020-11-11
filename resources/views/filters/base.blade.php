b8f12e082a0431b2ddef603e9f905cf0ad540488
heksen84 <heksen84@yandex.ru>
2020-11-11 18:46:12 +0600

    up+

--- resources/views/filters/base.blade.php
+++ resources/views/filters/base.blade.php
@@ -4,5 +4,10 @@
 	<form id="filters-form" action="/{{$region}}/{{$city}}/c/{{$category}}/{{$subcategory}}">
 @else
 	<form id="filters-form" action="/c/{{$category}}/{{$subcategory}}">
-@endif
+@endif	
+	<div class="form-row m-1 mt-2">
+		<div class="form-group col-4 col-sm-3 col-md-2 col-xl-2">
+      		<label>Цена:</label>
+			<input type="number" placeholder="от" id="price_ot" class="form-control form-control-sm" name="price_ot" value="{{ $filters['price_ot'] }}" required/>			
+		</div>													
 </form>
