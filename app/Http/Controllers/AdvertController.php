498f7fd772471679fa6dacee3ebdc508980dffd9
heksen84 <heksen84@yandex.ru>
2021-01-06 22:16:22 +0600

    up+

--- app/Http/Controllers/AdvertController.php
+++ app/Http/Controllers/AdvertController.php
@@ -35,62 +35,329 @@ class AdvertController extends Controller {
         return $this->new_advert_common("Подать объявление бесплатно", $request);
         }
 
-        // подать бесплатно объявление о работе в кз
+        // Подать бесплатно объявление о работе в кз
         public function podat_besplatno_obyavlenie_o_rabote_v_kz(Request $request) {                
-        return $this->new_advert_common("подать бесплатно объявление о работе в кз", $request);
+        return $this->new_advert_common("Подать бесплатно объявление о работе в кз", $request);
         }
 
-        // подать бесплатное объявление в усть каменогорске
+        // Подать бесплатное объявление в усть каменогорске
         public function podat_besplatnoe_obyavlenie_v_ust_kamenogorske(Request $request) {                           
-        return $this->new_advert_common("подать бесплатное объявление в усть каменогорске", $request);
+        return $this->new_advert_common("Подать бесплатное объявление в усть каменогорске", $request);
         }
 
-        // подать бесплатное объявление продажа гаража
+        // Подать бесплатное объявление продажа гаража
         public function podat_besplatnoe_obyavlenie_prodazha_garazha(Request $request) {                           
-        return $this->new_advert_common("подать бесплатное объявление продажа гаража", $request);
+        return $this->new_advert_common("Подать бесплатное объявление продажа гаража", $request);
         }
 
-        // подать бесплатные объявления аренда квартир
+        // Подать бесплатные объявления аренда квартир
         public function podat_besplatnye_obyavleniya_arenda_kvartir(Request $request) {                           
-        return $this->new_advert_common("подать бесплатные объявления аренда квартир", $request);
+        return $this->new_advert_common("Подать бесплатные объявления аренда квартир", $request);
         }
 
-        // подать детское объявление
+        // Подать детское объявление
         public function podat_detskoe_obyavlenie(Request $request) {                           
-        return $this->new_advert_common("подать детское объявление", $request);
+        return $this->new_advert_common("Подать детское объявление", $request);
         }
 
-        // подать объявление авто пробегом
+        // Подать объявление авто пробегом
         public function podat_obyavlenie_avto_probegom(Request $request) {                           
-        return $this->new_advert_common("подать объявление авто пробегом", $request);
+        return $this->new_advert_common("Подать объявление авто пробегом", $request);
         }
 
-        // подать объявление аренда
+        // Подать объявление аренда
         public function podat_obyavlenie_arenda(Request $request) {                           
-        return $this->new_advert_common("подать объявление аренда", $request);
+        return $this->new_advert_common("Подать объявление аренда", $request);
         }
 
-        // подать объявление бесплатно в области
+        // Подать объявление бесплатно в области
         public function podat_obyavlenie_besplatno_v_oblasti(Request $request) {                           
-        return $this->new_advert_common("подать объявление бесплатно в области", $request);
+        return $this->new_advert_common("Подать объявление бесплатно в области", $request);
         }
 
-        // подать объявление в газету работа
+        // Подать объявление в газету работа
         public function podat_obyavlenie_v_gazetu_rabota(Request $request) {                           
-        return $this->new_advert_common("подать объявление в газету работа", $request);
+        return $this->new_advert_common("Подать объявление в газету работа", $request);
         }                
 
-        // подать объявление в рубрике
+        // Подать объявление в рубрике
         public function podat_obyavlenie_v_rubrike(Request $request) {                           
-        return $this->new_advert_common("подать объявление в рубрике", $request);
+        return $this->new_advert_common("Подать объявление в рубрике", $request);
         }                
 
-        // подать объявление в усть каменогорске
+        // Подать объявление в усть каменогорске
         public function podat_obyavlenie_v_ust_kamenogorske(Request $request) {                           
-        return $this->new_advert_common("подать объявление в усть каменогорске", $request);
-        }                
+        return $this->new_advert_common("Подать объявление в усть каменогорске", $request);
+        }
 
+        // Подать объявление инфо
+        public function podat_obyavlenie_info(Request $request) {                           
+        return $this->new_advert_common("Подать объявление инфо", $request);
+        }
+
+        // Подать объявление мясо
+        public function podat_obyavlenie_myaso(Request $request) {                           
+        return $this->new_advert_common("Подать объявление мясо", $request);
+        }
+                
+        // Подать объявление на продавца
+        public function podat_obyavlenie_na_prodavca(Request $request) {                           
+        return $this->new_advert_common("Подать объявление на продавца", $request);
+        }
+        
+        // Подать объявление на продажу дома бесплатно
+        public function podat_obyavlenie_na_prodazhu_doma_besplatno(Request $request) {                           
+        return $this->new_advert_common("Подать объявление на продажу дома бесплатно", $request);
+        }
+        
+        // Подать объявление найти работу
+        public function podat_obyavlenie_nayti_rabotu(Request $request) {                           
+        return $this->new_advert_common("Подать объявление найти работу", $request);
+        }
+        
+        // Подать объявление насчет работы
+        public function podat_obyavlenie_naschet_raboty(Request $request) {                           
+        return $this->new_advert_common("Подать объявление насчет работы", $request);
+        }
+
+        // Подать объявление о наборе сотрудников
+        public function podat_obyavlenie_o_nabore_sotrudnikov(Request $request) {                           
+        return $this->new_advert_common("Подать объявление о наборе сотрудников", $request);
+        }
+        
+        // Подать объявление о наборе сотрудников бесплатно
+        public function podat_obyavlenie_o_nabore_sotrudnikov_besplatno(Request $request) {                           
+        return $this->new_advert_common("Подать объявление о наборе сотрудников бесплатно", $request);
+        }
+        
+        // Подать объявление о продаже авто бесплатно
+        public function podat_obyavlenie_o_prodazhe_avto_besplatno(Request $request) {                           
+        return $this->new_advert_common("Подать объявление о продаже авто бесплатно", $request);
+        }
+
+        // Подать объявление о продаже автомобиля бесплатно
+        public function podat_obyavlenie_o_prodazhe_avtomobilya_besplatno(Request $request) {                           
+        return $this->new_advert_common("Подать объявление о продаже автомобиля бесплатно", $request);
+        }
+
+        // Подать объявление о продаже б у
+        public function podat_obyavlenie_o_prodazhe_b_u(Request $request) {                           
+        return $this->new_advert_common("Подать объявление о продаже б у", $request);
+        }
+        
+        // Подать объявление о продаже дачи
+        public function podat_obyavlenie_o_prodazhe_dachi(Request $request) {                           
+        return $this->new_advert_common("Подать объявление о продаже дачи", $request);
+        }
+        
+        // Подать объявление о продаже машины
+        public function podat_obyavlenie_o_prodazhe_mashiny(Request $request) {                           
+        return $this->new_advert_common("Подать объявление о продаже машины", $request);
+        }
+        
+        // Подать объявление о продаже недвижимости
+        public function podat_obyavlenie_o_prodazhe_nedvizhimosti(Request $request) {                           
+        return $this->new_advert_common("Подать объявление о продаже недвижимости", $request);
+        }
+        
+        // Подать объявление о продаже шин
+        public function podat_obyavlenie_o_prodazhe_shin(Request $request) {                           
+        return $this->new_advert_common("Подать объявление о продаже шин", $request);
+        }
+        
+        // Подать объявление о пропаже документов
+        public function podat_obyavlenie_o_propazhe_dokumentov(Request $request) {                           
+        return $this->new_advert_common("Подать объявление о пропаже документов", $request);
+        }
+
+        // Подать объявление бесплатно в караганде
+        public function podat_obyavlenie_besplatno_v_karagande(Request $request) {                           
+        return $this->new_advert_common("Подать объявление бесплатно в караганде", $request);
+        }
+        
+        // Подать объявление о сдаче квартиры в аренду
+        public function podat_obyavlenie_o_sdache_kvartiry_v_arendu(Request $request) {                           
+        return $this->new_advert_common("Подать объявление о сдаче квартиры в аренду", $request);
+        }
+        
+        // Подать объявление об аренде квартиры
+        public function podat_obyavlenie_ob_arende_kvartiry(Request $request) {                           
+        return $this->new_advert_common("Подать объявление об аренде квартиры", $request);
+        }
+        
+        // Подать объявление куплю
+        public function podat_obyavlenie_kuplyu(Request $request) {                           
+        return $this->new_advert_common("Подать объявление куплю", $request);
+        }
+        
+        // Подать объявление о работе бесплатно
+        public function podat_obyavlenie_o_rabote_besplatno(Request $request) {
+        return $this->new_advert_common("Подать объявление о работе бесплатно", $request);
+        }
+        
+        // Подать объявление об услугах бесплатно
+        public function podat_obyavlenie_ob_uslugah_besplatno(Request $request) {
+        return $this->new_advert_common("Подать объявление об услугах бесплатно", $request);
+        }
+        
+        // Подать объявление онлайн
+        public function podat_obyavlenie_onlayn(Request $request) {
+        return $this->new_advert_common("Подать объявление онлайн", $request);
+        }
+        
+        // Подать объявление по английскому
+        public function podat_obyavlenie_po_angliyskomu(Request $request) {
+        return $this->new_advert_common("Подать объявление по английскому", $request);
+        }
+        
+        // Подать объявление продажа телефона
+        public function podat_obyavlenie_prodazha_telefona(Request $request) {
+        return $this->new_advert_common("Подать объявление продажа телефона", $request);
+        }
+        
+        // Подать объявление продажа техника
+        public function podat_obyavlenie_prodazha_tehnika(Request $request) {
+        return $this->new_advert_common("Подать объявление продажа техника", $request);
+        }
+        
+        // Подать объявление продажа щенков
+        public function podat_obyavlenie_prodazha_shchenkov(Request $request) {
+        return $this->new_advert_common("Подать объявление продажа щенков", $request);
+        }
+        
+        // Подать объявление продать авто
+        public function podat_obyavlenie_prodat_avto(Request $request) {
+        return $this->new_advert_common("Подать объявление продать авто", $request);
+        }
+        
+        // Подать объявление ремонт квартиры
+        public function podat_obyavlenie_remont_kvartiry(Request $request) {
+        return $this->new_advert_common("Подать объявление ремонт квартиры", $request);
+        }
+        
+        // Подать объявление сантехник
+        public function podat_obyavlenie_santehnik(Request $request) {
+        return $this->new_advert_common("Подать объявление сантехник", $request);
+        }
+        
+        // Подать объявление сантехника
+        public function podat_obyavlenie_santehnika(Request $request) {
+        return $this->new_advert_common("Подать объявление сантехника", $request);
+        }
+        
+        // Подать объявление строительные
+        public function podat_obyavlenie_stroitelnye(Request $request) {
+        return $this->new_advert_common("Подать объявление строительные", $request);
+        }
+        
+        // Подать объявление услуги электрика
+        public function podat_obyavlenie_uslugi_elektrika(Request $request) {
+        return $this->new_advert_common("Подать объявление услуги электрика", $request);
+        }
+        
+        // Подать объявления бытовой техники
+        public function podat_obyavleniya_bytovoy_tehniki(Request $request) {
+        return $this->new_advert_common("Подать объявления бытовой техники", $request);
+        }
+        
+        // Подать объявления доска бесплатных объявлений
+        public function podat_obyavleniya_doska_besplatnyh_obyavleniy(Request $request) {
+        return $this->new_advert_common("Подать объявления доска бесплатных объявлений", $request);
+        }
+        
+        // Подать объявления строительных работ
+        public function podat_obyavleniya_stroitelnyh_rabot(Request $request) {
+        return $this->new_advert_common("Подать объявления строительных работ", $request);
+        }
+        
+        // Подать объявления электрика
+        public function podat_obyavleniya_elektrika(Request $request) {
+        return $this->new_advert_common("Подать объявления электрика", $request);
+        }
+        
+        // Подать платное объявление
+        public function podat_platnoe_obyavlenie(Request $request) {
+        return $this->new_advert_common("Подать платное объявление", $request);
+        }
+        
+        // Сервис разместить объявление
+        public function servis_razmestit_obyavlenie(Request $request) {
+        return $this->new_advert_common("Сервис разместить объявление", $request);
+        }
+        
+        // Сиделка подать объявление
+        public function sidelka_podat_obyavlenie(Request $request) {
+        return $this->new_advert_common("Сиделка подать объявление", $request);
+        }
+        
+        // Стройка подать объявление
+        public function stroyka_podat_obyavlenie(Request $request) {
+        return $this->new_advert_common("Стройка подать объявление", $request);
+        }
+        
+        // Хочу купить подать объявление
+        public function hochu_kupit_podat_obyavlenie(Request $request) {
+        return $this->new_advert_common("Хочу купить подать объявление", $request);
+        }
+        
+        // Юридические услуги подать объявление
+        public function yuridicheskie_uslugi_podat_obyavlenie(Request $request) {
+        return $this->new_advert_common("Юридические услуги подать объявление", $request);
+        }
+        
+        // Акжайык подать объявление
+        public function akzhayyk_podat_obyavlenie(Request $request) {
+        return $this->new_advert_common("Акжайык подать объявление", $request);
+        }
+        
+        // Газета срочно подать объявление
+        public function gazeta_srochno_podat_obyavlenie(Request $request) {
+        return $this->new_advert_common("Газета срочно подать объявление", $request);
+        }
+        
+        // Газеты области подать объявления
+        public function gazety_oblasti_podat_obyavleniya(Request $request) {
+        return $this->new_advert_common("Газеты области подать объявления", $request);
+        }
+        
+        // Нурсултан подать объявление на продажу автомобиля
+        public function nursultan_podat_obyavlenie_na_prodazhu_avtomobilya(Request $request) {
+        return $this->new_advert_common("Нурсултан подать объявление на продажу автомобиля", $request);
+        }
+        
+        // Работа в интернете разместить объявление
+        public function rabota_v_internete_razmestit_obyavlenie(Request $request) {
+        return $this->new_advert_common("Работа в интернете разместить объявление", $request);
+        }
+        
+        // Работа частные объявления подать
+        public function rabota_chastnye_obyavleniya_podat(Request $request) {
+        return $this->new_advert_common("Работа частные объявления подать", $request);
+        }
+        
+        // Разместить объявление бесплатно и без регистрации
+        public function razmestit_obyavlenie_besplatno_i_bez_registracii(Request $request) {
+        return $this->new_advert_common("Разместить объявление бесплатно и без регистрации", $request);
+        }
+        
+        // Разместить объявление о продаже
+        public function razmestit_obyavlenie_o_prodazhe(Request $request) {
+        return $this->new_advert_common("Разместить объявление о продаже", $request);
+        }
+        
+        // Разместить объявление о работе
+        public function razmestit_obyavlenie_o_rabote(Request $request) {
+        return $this->new_advert_common("Разместить объявление о работе", $request);
+        }
+        
+        // Разместить объявление о работе бесплатно
+        public function razmestit_obyavlenie_o_rabote_besplatno(Request $request) {
+        return $this->new_advert_common("Разместить объявление о работе бесплатно", $request);
+        }        
+
+        // ----------------------------------
         // ???
+        // ----------------------------------
         public function posted($url) {
 	 return view("adposted")->with( "url", $url );
         }
