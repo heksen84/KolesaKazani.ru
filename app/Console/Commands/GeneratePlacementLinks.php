8595528a70334b3ae52fb47f2a173afae53f7a0d
heksen84 <heksen84@yandex.ru>
2021-02-15 01:39:47 +0600

    up+

--- app/Console/Commands/GeneratePlacementLinks.php
+++ app/Console/Commands/GeneratePlacementLinks.php
@@ -48,31 +48,58 @@ class GenerateSitemapLinks extends Command
 
         $app_url = config('app.url', 'Laravel');
 
-        $date_time = Carbon::now()->format('Y-m-d');        
+        $date = Carbon::now()->format('Y-m-d');        
 
-        $this->info($date_time);
+        $this->info($date);
         
-        $file  = fopen("razmeshenye.xml", "w");
+        $file = fopen("razmeshenye.xml", "w");
 
 	fwrite($file, '<?xml version="1.0" encoding="UTF-8"?>');
         fwrite($file,'<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">');
         
         $places = Places::select("name", "url")->orderBy('name')->distinct()->get();	
 
+/*  <url>
+    <loc>https://ilbo.site/podat-obyavlenie-besplatno-astana</loc>
+    <lastmod>2021-01-19</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.8</priority>
+  </url>*/
+
 	foreach($places as $place) {          
-	  fwrite($file, str2url("Подать объявление ".$place->name)."\n");	 		  
+	  fwrite($file, "<url>\n");	 		  
+	  fwrite($file, "<loc>".$app_url."/".str2url("Подать объявление ".$place->name)."</loc>\n");	 		  
+	  fwrite($file, "<lastmod>".$date."</lastmod>\n");
+	  fwrite($file, "<changefreq>daily</changefreq>\n");
+	  fwrite($file, "<priority>0.8</priority>\n");
+	  fwrite($file, "</url>\n");	 		  
         }
         
-	foreach($places as $place) {          
-	 fwrite($file, str2url("Подать объявление бесплатно ".$place->name)."\n");
+	foreach($places as $place) {
+	  fwrite($file, "<url>\n");	 		  
+	  fwrite($file, "<loc>".$app_url."/".str2url("Подать объявление бесплатно ".$place->name)."</loc>\n");	 		  
+	  fwrite($file, "<lastmod>".$date."</lastmod>\n");
+	  fwrite($file, "<changefreq>daily</changefreq>\n");
+	  fwrite($file, "<priority>0.8</priority>\n");
+	  fwrite($file, "</url>\n");	 		  
+	}
+
+	foreach($places as $place) {
+	  fwrite($file, "<url>\n");	 		  
+	  fwrite($file, "<loc>".$app_url."/".str2url("Разместить объявление ".$place->name)."</loc>\n");	 		  
+	  fwrite($file, "<lastmod>".$date."</lastmod>\n");
+	  fwrite($file, "<changefreq>daily</changefreq>\n");
+	  fwrite($file, "<priority>0.8</priority>\n");
+	  fwrite($file, "</url>\n");	 		  
 	}
 
 	foreach($places as $place) {          
-	  fwrite($file, str2url("Разместить объявление ".$place->name)."\n");
-	}
-
-	foreach($places as $place) {          
-	  fwrite($file, str2url("Разместить объявление бесплатно ".$place->name)."\n");
+	  fwrite($file, "<url>\n");	 		  
+	  fwrite($file, "<loc>".$app_url."/".str2url("Разместить объявление бесплатно ".$place->name)."</loc>\n");	 		  
+	  fwrite($file, "<lastmod>".$date."</lastmod>\n");
+	  fwrite($file, "<changefreq>daily</changefreq>\n");
+	  fwrite($file, "<priority>0.8</priority>\n");
+	  fwrite($file, "</url>\n");	 		  
 	}
 
         fwrite($file, '</urlset>');
