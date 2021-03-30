bf54e5ca1c72eede660943786d4a4db34b33f914
heksen84 <heksen84@yandex.ru>
2021-03-30 21:12:27 +0600

    up+

--- app/Http/Controllers/xml.php
+++
@@ -1,7 +0,0 @@
-<?php
-$string = "<element><child>Hello World</child></element>";
-$xml = new SimpleXMLElement($string);
-
-// The entire XML tree as a string:
-// "<element><child>Hello World</child></element>"
-echo $xml->asXML();
