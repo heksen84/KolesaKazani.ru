9d433ca619c5f766ce1766ab4817d0b9027a8b32
heksen84 <heksen84@yandex.ru>
2021-05-11 11:09:12 +0600

    up+

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,8 +13,7 @@ class AdminController extends Controller
     public function parseOlx() {
 
         $outout = "";
-        exec("ls -la", $output);
-
+        exec("php artisan parse:olx", $output);
         
         return $output;
     }
