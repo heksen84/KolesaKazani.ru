57a9ed6bc68d6c10ef404da4ec09ac6e4bb1ce50
heksen84 <heksen84@yandex.ru>
2021-05-11 11:48:58 +0600

    up+

--- app/Console/Commands/ParseOlx.php
+++ app/Console/Commands/ParseOlx.php
@@ -159,7 +159,6 @@ class ParseOlx extends Command {
     $this->info("img_real_path: ".storage_path("app")."/".$imgRealPath);
     $this->info("img_original_name: ".$imgOriginalName);
 
-
     $dataNursultanPhonesAndGadgets = array(
         "uid" => $this->makeid(10),
         "adv_optype"=> $optype,        
@@ -228,10 +227,28 @@ class ParseOlx extends Command {
         "img_original_name" => $imgOriginalName,
     );
 
+    $dataAksuModaAndStyle = array(
+        "uid" => $this->makeid(10),
+        "adv_optype"=> $optype,
+        "region_id" => 11,
+        "city_id" => 50,
+        "adv_category" => 6,        
+        "adv_subcategory" => 33,
+        "adv_info" => $desc,
+        "adv_price" => $price,
+        "adv_phone" => $phoneNumber,
+        "adv_title" => $title,
+        "adv_coords" => "52.040616,76.926367", // Pavl, Aksu        
+        "olx_id" => $advertId,
+        "img_real_path" => storage_path("app")."/".$imgRealPath,
+        "img_original_name" => $imgOriginalName,
+    );
+
     $dataArray = [];
 
     //array_push($dataArray, $dataAksuComps);
-    array_push($dataArray, $dataAksuPhonesAndGadgets);
+//    array_push($dataArray, $dataNursultanPhonesAndGadgets);
+    array_push($dataArray, $dataAksuModaAndStyle);
 
     foreach($dataArray as $data) {
         //$this->info("-----------------------------------------");
@@ -262,8 +279,9 @@ class ParseOlx extends Command {
 
        //$page = self::getPage("https://www.olx.kz/elektronika/kompyutery-i-komplektuyuschie/nastolnye-kompyutery/aksu_5689/", $cookie);
        //$page = self::getPage("https://www.olx.kz/elektronika/kompyutery-i-komplektuyuschie/nastolnye-kompyutery/astana/", $cookie);
-       $page = self::getPage("https://www.olx.kz/elektronika/telefony-i-aksesuary/mobilnye-telefony-smartfony/aksu_5689/", $cookie);
+       //$page = self::getPage("https://www.olx.kz/elektronika/telefony-i-aksesuary/mobilnye-telefony-smartfony/aksu_5689/", $cookie);	
        //$page = self::getPage("https://www.olx.kz/elektronika/telefony-i-aksesuary/mobilnye-telefony-smartfony/astana/", $cookie);
+       $page = self::getPage("https://www.olx.kz/moda-i-stil/aksessuary/aksu_5689/", $cookie);
 
         $this->info("ok\n");
 
