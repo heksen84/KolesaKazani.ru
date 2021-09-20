475b2ca7c2c8246dd64db83b2282e3af0de3cd21
heksen84 <heksen84@yandex.ru>
2021-09-21 00:58:20 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -139,12 +139,7 @@
   <script>
 
   function displayMenu(event) {
-    if (document.getElementById("navbar-list").classList.contains("show")) {
-        document.getElementById("navbar-list").classList.remove("show")
-    }
-    else {
-        document.getElementById("navbar-list").classList.add("show")
-    }
+    document.getElementById("navbar-list").classList.toggle("show");
   }
 
   function showAll() {      
