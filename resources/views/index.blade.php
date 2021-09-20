50d7d8049d79a8a93ab9d9f9e18bfea14415cce7
heksen84 <heksen84@yandex.ru>
2021-09-20 12:04:15 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -62,41 +62,6 @@
   </nav>
   </div>
 
-  <div class="modal fade" id="locationModal" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-      <div class="modal-content" style="word-wrap: break-word">
-        <div class="modal-header"><button type="button" class="close closeLocationWindow" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>
-          <div class="modal-body">   
-            <input type="text" class="form-control mb-2" placeholder="Введите местоположение поиска, например Нур-Султан" id="placeFilter"></input>
-              
-              <div id="placeData">
-                <div id="regions">                
-                  <div style="text-align:center"><a href="/" class="grey link" style="background:yellow;margin:auto">Искать по Казахстану</a></div>               	                
-                    <div class="mt-2">
-                      @foreach($regions as $region)
-                        <a href=/{{ $region["url"] }} class="grey link text-center region_link" id={{ $region["region_id"] }}><div class="mt-1">{{ $region["name"] }} обл.</div></a>
-                      @endforeach                    
-                      <div class="text-center">
-                        <button class='btn btn-sm btn-success m-3 closeLocationWindow'>Отмена</button>
-                      </div>                    
-                    </div>
-                  </div>
-                  <div id="loaderForSearchPlace">
-                    <div class="d-flex justify-content-center">
-                      <button class="btn btn-primary" type="button" disabled>
-                        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>        
-                      </button>
-                    </div>
-                  </div>
-                <div id="places" class="text-center hide"></div>
-              </div>
-              <div id="placeSearchResults"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
 
 <!--<div style="float:left" class="index-side-advert ml-4 mt-2">
   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
@@ -195,16 +160,9 @@
         
       <!-- компонент поиска по сайту -->
       <form action="/search" method="get">
-        <input type="text" id="search_string" placeholder="поиск по объявлениям" name="searchString" required/>
-        <input type="text" name="region" value="{{ $urlRegion }}" hidden/>
-        <input type="text" name="place" value="{{ $urlPlace }}" hidden/>
+        <input type="text" id="search_string" placeholder="поиск по объявлениям" name="searchString"/>
         <button id="button_search" type="submit" title="Найти что требуется">найти</button>
       </form>
-
-      <!-- кнопки выбора региона и т.п.-->
-<!--     <div class="index_select_region_and_other_button_block">    
-        <button class="btn btn-link" data-toggle="modal" id="locationButton" style="margin-top:-8px">Расположение {{ $locationName }} <span title="Выбрать раположения поиска">(изменить)</span></button>
-     </div> -->
 
 
       </div>
@@ -257,187 +215,23 @@
     </div>
 	
     <br>
-    Все марки
-  <!--<div id="categories_line">
-    <div class="center">        
-      <div id="categories_title" class="shadow_text"></div>    
-	      <div class="form-inline" id="categories">
-          @foreach($categories as $category)
-        	  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-3 col_item" id="{{ $category['id'] }}">                      	   
-              @if ($location==="/")
-                <a href="/c/{{ $category['url'] }}" class="url"><div class="category_item">{{ $category["name"] }}</div></a>
-              @else
-                <a href="/{{ $location }}/c/{{ $category['url'] }}" class="url"><div class="category_item">{{ $category["name"] }}</div></a>
-              @endif
-        	  </div>
-          @endforeach
-	      </div>  
-
-        <div id="subcats">
-         <button type="button" id="close_subcats_btn" class="btn btn-link">&#8634; назад</button>         
-          <div id="subcategories" class="form-inline center">
-            @foreach($subcategories as $subcategory)
-              <div class="col-sm-12 col-md-12 col-lg-12 col-xl-3 hide" data-category-id="{{ $subcategory['category_id'] }}">
-              @if ($location==="/")
-                <a href="/c/{{ $subcategory['category_url']}}/{{ $subcategory['url'] }}" class="url"><div class="category_item subcategory">{{ $subcategory["name"] }}</div></a>
-              @else
-                <a href="/{{ $location }}/c/{{ $subcategory['category_url'] }}/{{ $subcategory['url'] }}" class="url"><div class="category_item subcategory">{{ $subcategory["name"] }}</div></a>
-              @endif
-              </div>
-            @endforeach
-          </div>
-        </div>        
-      </div>
-    </div>
-  <br>
-
-  @if ( count($topAdverts) > 0 )
-  <div class="row text-center">
-    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-2">      
-      <div class="titleAdverts">ТОП объявления</div>  
-    </div>
-      @foreach($topAdverts as $advert)      
-      <div class="col-sm-12 col-md-3 col-lg-3 col-xl-2 m-3">      
-        <a href="/objavlenie/show/{{ $advert->url }}" class="black">
-          @if ($advert->color)
-            <div class="card index-card green-background">
-          @else
-            <div class="card index-card">
-          @endif
-          @if ($advert->srochno)        
-            <div class="label-torg">срочно</div>
-          @endif
-            <img class="card-img-top" src="{{ $advert->imageName }}" onerror="this.onerror=null;this.src='/public/images/_nofoto.jpg';" loading="lazy">          
-              <div class="card-title-text">{{ $advert->title }}</div>
-              <div class="card-location-text">{{ $advert->region_name }}<br>{{ $advert->city_name }}</div>
-              <b class="card-price-value">{{ $advert->price }} ₸</b>
-          </div>
-        </a>
-      </div>      
-      @endforeach
-  </div>
-  @endif
-  
-  @if ( count( $newAdverts ) > 0 )
-  <div class="row text-center">
-    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 mt-1">      
-      <div class="titleAdverts">новые объявления</div>  
-    </div>
-      @foreach($newAdverts as $advert)      
-      <div class="col-sm-12 col-md-3 col-lg-3 col-xl-2 m-3">      
-        <a href="/objavlenie/show/{{ $advert->url }}" class="black">
-          @if ($advert->color)
-            <div class="card index-card green-background">
-          @else
-            <div class="card index-card">
-          @endif
-          @if ($advert->srochno)        
-            <div class="label-torg">срочно</div>
-          @endif
-            <img class="card-img-top" src="{{ $advert->imageName }}" onerror="this.onerror=null;this.src='/public/images/_nofoto.jpg';" loading="lazy">          
-              <div class="card-title-text">{{ $advert->title }}</div>
-              <div class="card-location-text">{{ $advert->region_name }} обл.<br>{{ $advert->city_name }}</div>
-              @if ($advert->price)
-                <b class="card-price-value">{{ $advert->price }} ₸</b>
-              @endif
-          </div>
-        </a>
-      </div>      
-      @endforeach
-  </div>
-  @endif  -->  
-  
-  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center mt-1 mb-1">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>  
-      <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-8074944108437227"
-        data-ad-format="auto"
-        data-ad-slot="8746851039"        
-        data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-  </div>
+    Все марки 
 
   <div class="text-center m-4 ">
     <a href="/podat-objavlenie" class="black" style="font-size:22px;letter-spacing:4px;font-weight:300">подать объявление</a>
   </div>
 
-<!-- РЕКЛАМА -->
-<!--  <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center mt-5">
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>  
-  <ins class="adsbygoogle"
-     style="display:inline-block;width:100%;height:130px"
-     data-full-width-responsive="true"
-     data-ad-client="ca-pub-8074944108437227"
-     data-ad-slot="2249357572"></ins>
-    <script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-  </div>-->
+  <div class="row mt-2">          
+    <div id="footer">
+		  <a href="/rules" class="underline_link">Правила сайта</a> | <a href="/about" class="underline_link">О сайте</a> 	 
+	  </div>        
+  </div>
 
-  <!--<div class="row mt-5">
-   <div class="col text-center">
-    <div style="letter-spacing:2px">ИЛЬБО в соц. сетях:</div>
-     <a href="https://vk.com/ilbo_kz"><img src="/public/images/social/vk.svg"></img></a>
-     <a href="https://ok.ru/group/58900893401207"><img src="/public/images/social/ok.svg"></img></a>
-     <a href="https://www.instagram.com/ilbo_kz"><img src="/public/images/social/insta.svg" width="56" height="56" style="margin-left:-5px"></img></a>     
-    </div>
-  </div>-->
-
-        <div class="row mt-2">          
-          <div id="footer">
-		        <a href="/rules" class="underline_link">Правила сайта</a> | <a href="/about" class="underline_link">О сайте</a> | <a href="https://kojan.kz" class="underline_link">Информационно-развлекательный портал Коян</a>
-  	        <br>
-		        <a href="https://webmaster.yandex.ru/siteinfo/?site=https://ilbo.site"><img width="88" height="31" alt="" border="0" src="https://yandex.ru/cycounter?https://ilbo.site&theme=light&lang=ru"/></a>
-	      </div>
-        
-        </div>
-	<script type="text/javascript" src="{{ mix('js/manifest.js') }}"></script>        
-	<script type="text/javascript" src="{{ mix('js/vendor.js') }}"></script>        
-        <script type="text/javascript" src="{{ mix('js/index.js') }}"></script>
+	    <script type="text/javascript" src="{{ mix('js/manifest.js') }}"></script>        
+	    <script type="text/javascript" src="{{ mix('js/vendor.js') }}"></script>        
+      <script type="text/javascript" src="{{ mix('js/index.js') }}"></script>
       </div>
     </div>
-  </body>
-
-  <!-- Yandex.Metrika counter -->
-  <script type="text/javascript" >
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-    ym(68275672, "init", {
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true,
-          webvisor:true
-     });
-  </script>
-  <noscript><div><img src="https://mc.yandex.ru/watch/68275672" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-  <!-- /Yandex.Metrika counter -->
-
-  <!-- google adscence -->
-  <script data-ad-client="ca-pub-8074944108437227" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-
-  <!-- ZERO.KZ -->
-  <script type="text/javascript">
-    var _zero_kz_ = _zero_kz_ || [];
-    _zero_kz_.push(["id", 72991]);
-    // Цвет кнопки
-    _zero_kz_.push(["type", 1]);
-    // Проверять url каждые 200 мс, при изменении перегружать код счётчика
-    // _zero_kz_.push(["url_watcher", 200]);
-
-    (function () {
-    var a = document.getElementsByTagName("script")[0],
-    s = document.createElement("script");
-    s.type = "text/javascript";
-    s.async = true;
-    s.src = (document.location.protocol == "https:" ? "https:" : "http:")
-    + "//c.zero.kz/z.js";
-    a.parentNode.insertBefore(s, a);
-    })();
-  </script>
+  </body>  
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -34,24 +34,6 @@ Route::get("/moderator", "ModeratorController@showHomePage");
 // детали объявления
 Route::get("/objavlenie/show/{title}", "DetailsController@getDetails");
 Route::get("/objavlenie/posted/{url}", "AdvertController@posted");
-
-// ----------------------------------------------------
-// Генерирую url из таблицы
-// ----------------------------------------------------
-/*foreach(SE_UserQueries::All() as $item) {    
-
-    Route::get("/".str_replace(' ', '-',$item->title), [ 
-        "uses"=>"UrlGeneratorController@convertData", 
-        "title"=>$item->title,
-        "category_id"=>$item->category_id,
-        "subcategory_id"=>$item->subcategory_id,
-        "optype"=>$item->optype,
-        "region_id"=>$item->region_id,
-        "place_id"=>$item->place_id,
-        "view"=>"вьюха"
-    ]);
-}*/
-
 Route::get("/sitemap/test.xml", "SitemapController@getUrls");
 
 /* 
@@ -60,134 +42,7 @@ Route::get("/sitemap/test.xml", "SitemapController@getUrls");
 ------------------------------------------------------------------------------------------------------------------------*/
 // Подать объявление
 Route::get("/podat-objavlenie", "AdvertController@new_advert");
-// Подать бесплатно объявление о работе в кз
-Route::get("/podat-besplatno-objavlenye-o-rabote-v-kz", "AdvertController@podat_besplatno_obyavlenie_o_rabote_v_kz");
-// Подать бесплатное объявление в усть каменогорске
-Route::get("/podat-besplatnoe-objavlenye-v-ust-kamenogorske", "AdvertController@podat_besplatnoe_obyavlenie_v_ust_kamenogorske");
-// Подать бесплатное объявление продажа гаража
-Route::get("/podat-besplatnoe-obyavlenie-prodazha-garazha", "AdvertController@podat_besplatnoe_obyavlenie_prodazha_garazha");
-// Подать бесплатные объявления аренда квартир
-Route::get("/podat-besplatnye-obyavleniya-arenda-kvartir", "AdvertController@podat_besplatnye_obyavleniya_arenda_kvartir");
-// Подать детское объявление
-Route::get("/podat-detskoe-obyavlenie", "AdvertController@podat_detskoe_obyavlenie");
-// Подать объявление авто пробегом
-Route::get("/podat-obyavlenie-avto-probegom", "AdvertController@podat_obyavlenie_avto_probegom");
-// Подать объявление аренда
-Route::get("/podat-obyavlenie-arenda", "AdvertController@podat_obyavlenie_arenda");
-// Подать объявление бесплатно в области
-Route::get("/podat-obyavlenie-besplatno-v-oblasti", "AdvertController@podat_obyavlenie_besplatno_v_oblasti");
-// Подать объявление в газету работа
-Route::get("/podat-obyavlenie-v-gazetu-rabota", "AdvertController@podat_obyavlenie_v_gazetu_rabota");
-// Подать объявление в рубрике
-Route::get("/podat-obyavlenie-v-rubrike", "AdvertController@podat_obyavlenie_v_rubrike");
-// Подать объявление в усть каменогорске
-Route::get("/podat-obyavlenie-v-ust-kamenogorske", "AdvertController@podat_obyavlenie_v_ust_kamenogorske");
-// Подать объявление инфо
-Route::get("/podat-obyavlenie-info", "AdvertController@podat_obyavlenie_info");
-// Подать объявление мясо
-Route::get("/podat-obyavlenie-myaso", "AdvertController@podat_obyavlenie_myaso");
-// Подать объявление на продавца
-Route::get("/podat-obyavlenie-na-prodavca", "AdvertController@podat_obyavlenie_na_prodavca");
-// Подать объявление на продажу дома бесплатно
-Route::get("/podat-obyavlenie-na-prodazhu-doma-besplatno", "AdvertController@podat_obyavlenie_na_prodazhu_doma_besplatno");
-// Подать объявление найти работу
-Route::get("/podat-obyavlenie-nayti-rabotu", "AdvertController@podat_obyavlenie_nayti_rabotu");
-// Подать объявление насчет работы
-Route::get("/podat-obyavlenie-naschet-raboty", "AdvertController@podat_obyavlenie_naschet_raboty");
-// Подать объявление о наборе сотрудников
-Route::get("/podat-obyavlenie-o-nabore-sotrudnikov", "AdvertController@podat_obyavlenie_o_nabore_sotrudnikov");
-// Подать объявление о наборе сотрудников бесплатно
-Route::get("/podat-obyavlenie-o-nabore-sotrudnikov-besplatno", "AdvertController@podat_obyavlenie_o_nabore_sotrudnikov_besplatno");
-// Подать объявление о продаже авто бесплатно
-Route::get("/podat-obyavlenie-o-prodazhe-avto-besplatno", "AdvertController@podat_obyavlenie_o_prodazhe_avto_besplatno");
-// Подать объявление о продаже автомобиля бесплатно
-Route::get("/podat-obyavlenie-o-prodazhe-avtomobilya-besplatno", "AdvertController@podat_obyavlenie_o_prodazhe_avtomobilya_besplatno");
-// Подать объявление о продаже б у
-Route::get("/podat-obyavlenie-o-prodazhe-b-u", "AdvertController@podat_obyavlenie_o_prodazhe_b_u");
-// Подать объявление о продаже дачи
-Route::get("/podat-obyavlenie-o-prodazhe-dachi", "AdvertController@podat_obyavlenie_o_prodazhe_dachi");
-// Подать объявление о продаже машины
-Route::get("/podat-obyavlenie-o-prodazhe-mashiny", "AdvertController@podat_obyavlenie_o_prodazhe_mashiny");
-// Подать объявление о продаже недвижимости
-Route::get("/podat-obyavlenie-o-prodazhe-nedvizhimosti", "AdvertController@podat_obyavlenie_o_prodazhe_nedvizhimosti");
-// Подать объявление о продаже шин
-Route::get("/podat-obyavlenie-o-prodazhe-shin", "AdvertController@podat_obyavlenie_o_prodazhe_shin");
-// Подать объявление о пропаже документов
-Route::get("/podat-obyavlenie-o-propazhe-dokumentov", "AdvertController@podat_obyavlenie_o_propazhe_dokumentov");
-// Подать объявление бесплатно в караганде
-Route::get("/podat-obyavlenie-besplatno-v-karagande", "AdvertController@podat_obyavlenie_besplatno_v_karagande");
-// Подать объявление о сдаче квартиры в аренду
-Route::get("/podat-obyavlenie-o-sdache-kvartiry-v-arendu", "AdvertController@podat_obyavlenie_o_sdache_kvartiry_v_arendu");
-// Подать объявление об аренде квартиры
-Route::get("/podat-obyavlenie-ob-arende-kvartiry", "AdvertController@podat_obyavlenie_ob_arende_kvartiry");
-// Подать объявление куплю
-Route::get("/podat-obyavlenie-kuplyu", "AdvertController@podat_obyavlenie_kuplyu");
-// Подать объявление о работе бесплатно
-Route::get("/podat-obyavlenie-o-rabote-besplatno", "AdvertController@podat_obyavlenie_o_rabote_besplatno");
-// Подать объявление об услугах бесплатно
-Route::get("/podat-obyavlenie-ob-uslugah-besplatno", "AdvertController@podat_obyavlenie_ob_uslugah_besplatno");
-// Подать объявление онлайн
-Route::get("/podat-obyavlenie-onlayn", "AdvertController@podat_obyavlenie_onlayn");
-// Подать объявление по английскому
-Route::get("/podat-obyavlenie-po-angliyskomu", "AdvertController@podat_obyavlenie_po_angliyskomu");
-// Подать объявление продажа телефона
-Route::get("/podat-obyavlenie-prodazha-telefona", "AdvertController@podat_obyavlenie_prodazha_telefona");
-// Подать объявление продажа техника
-Route::get("/podat-obyavlenie-prodazha-tehnika", "AdvertController@podat_obyavlenie_prodazha_tehnika");
-// Подать объявление продажа щенков
-Route::get("/podat-obyavlenie-prodazha-shchenkov", "AdvertController@podat_obyavlenie_prodazha_shchenkov");
-// Подать объявление продать авто
-Route::get("/podat-obyavlenie-prodat-avto", "AdvertController@podat_obyavlenie_prodat_avto");
-// Подать объявление ремонт квартиры
-Route::get("/podat-obyavlenie-remont-kvartiry", "AdvertController@podat_obyavlenie_remont_kvartiry");
-// Подать объявление сантехник
-Route::get("/podat-obyavlenie-santehnik", "AdvertController@podat_obyavlenie_santehnik");
-// Подать объявление сантехника
-Route::get("/podat-obyavlenie-santehnika", "AdvertController@podat_obyavlenie_santehnika");
-// Подать объявление строительные
-Route::get("/podat-obyavlenie-stroitelnye", "AdvertController@podat_obyavlenie_stroitelnye");
-// Подать объявление услуги электрика
-Route::get("/podat-obyavlenie-uslugi-elektrika", "AdvertController@podat_obyavlenie_uslugi_elektrika");
-// Подать объявления бытовой техники
-Route::get("/podat-obyavleniya-bytovoy-tehniki", "AdvertController@podat_obyavleniya_bytovoy_tehniki");
-// Подать объявления доска бесплатных объявлений
-Route::get("/podat-obyavleniya-doska-besplatnyh-obyavleniy", "AdvertController@podat_obyavleniya_doska_besplatnyh_obyavleniy");
-// Подать объявления строительных работ
-Route::get("/podat-obyavleniya-stroitelnyh-rabot", "AdvertController@podat_obyavleniya_stroitelnyh_rabot");
-// Подать объявления электрика
-Route::get("/podat-obyavleniya-elektrika", "AdvertController@podat_obyavleniya_elektrika");
-// Подать платное объявление
-Route::get("/podat-platnoe-obyavlenie", "AdvertController@podat_platnoe_obyavlenie");
-// сервис разместить объявление
-Route::get("/servis-razmestit-obyavlenie", "AdvertController@servis_razmestit_obyavlenie");
-// сиделка Подать объявление
-Route::get("/sidelka-podat-obyavlenie", "AdvertController@sidelka_podat_obyavlenie");
-// Стройка подать объявление
-Route::get("/stroyka-podat-obyavlenie", "AdvertController@stroyka_podat_obyavlenie");
-// Хочу купить подать объявление
-Route::get("/hochu-kupit-podat-obyavlenie", "AdvertController@hochu_kupit_podat_obyavlenie");
-// Юридические услуги подать объявление
-Route::get("/yuridicheskie-uslugi-podat-obyavlenie", "AdvertController@yuridicheskie_uslugi_podat_obyavlenie");
-// Акжайык подать объявление
-Route::get("/akzhayyk-podat-obyavlenie", "AdvertController@akzhayyk_podat_obyavlenie");
-// Газета срочно подать объявление
-Route::get("/gazeta-srochno-podat-obyavlenie", "AdvertController@gazeta_srochno_podat_obyavlenie");
-// Газеты области подать объявления
-Route::get("/gazety-oblasti-podat-obyavleniya", "AdvertController@gazety_oblasti_podat_obyavleniya");
-// Нурсултан подать объявление на продажу автомобиля
-Route::get("/nursultan-podat-obyavlenie-na-prodazhu-avtomobilya", "AdvertController@nursultan_podat_obyavlenie_na_prodazhu_avtomobilya");
-// Работа в интернете разместить объявление
-Route::get("/rabota-v-internete-razmestit-obyavlenie", "AdvertController@rabota_v_internete_razmestit_obyavlenie");
-// Работа частные объявления подать
-Route::get("/rabota-chastnye-obyavleniya-podat", "AdvertController@rabota_chastnye_obyavleniya_podat");
-// Разместить объявление бесплатно и без регистрации
-Route::get("/razmestit-obyavlenie-besplatno-i-bez-registracii", "AdvertController@razmestit_obyavlenie_besplatno_i_bez_registracii");
-// Разместить объявление о продаже
-Route::get("/razmestit-obyavlenie-o-prodazhe", "AdvertController@razmestit_obyavlenie_o_prodazhe");
-// Разместить объявление о работе
-Route::get("/razmestit-obyavlenie-o-rabote", "AdvertController@razmestit_obyavlenie_o_rabote");
-// Разместить объявление о работе бесплатно
-Route::get("/razmestit-obyavlenie-o-rabote-besplatno", "AdvertController@razmestit_obyavlenie_o_rabote_besplatno");
+
 
 
 
@@ -239,7 +94,7 @@ Route::get("{region}/c/auto/{mark}{model}", "AutoResultsController@getCountryCat
 Route::get("{region}/{city}/c/auto/{mark}{model}", "AutoResultsController@getCityCategoryResults");*/
 
 // Главная страница
-Route::get("/", "IndexController@ShowCountryIndexPage");
+Route::get("/", "IndexController@ShowIndexPage");
 // мои объявления
 Route::get("/home", "HomeController@ShowHomePage");
 // Результаты по категориям по стране
