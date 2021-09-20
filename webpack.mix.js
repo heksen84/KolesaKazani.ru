f8a045d0218d531dd727719c4603eb65dd5a4e44
heksen84 <heksen84@yandex.ru>
2021-09-21 01:37:34 +0600

    up+

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,7 +18,7 @@ mix.js('resources/assets/js/mix/newad.js', 'public/js').
 mix.js('resources/assets/js/mix/results.js', 'public/js').
 mix.js('resources/assets/js/mix/details.js', 'public/js').
 mix.js('resources/assets/js/mix/home.js', 'public/js').
-mix.js('resources/assets/js/mix/moderator.js', 'public/js').extract(['vue', 'jquery', 'bootstrap', 'axios']).
+mix.js('resources/assets/js/mix/moderator.js', 'public/js').extract(['vue', 'bootstrap', 'axios']).
 sass('resources/assets/sass/common.scss', 'public/css').
 sass('resources/assets/sass/index.scss', 'public/css').
 sass('resources/assets/sass/newad.scss', 'public/css').
