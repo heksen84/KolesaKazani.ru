0de1ddebbed96820004207b6692f8495c3ce1658
heksen84 <heksen84@yandex.ru>
2021-09-21 00:54:06 +0600

    up+

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,7 +40,7 @@
     <div class="collapse navbar-collapse" id="navbar-list">
       <ul class="navbar-nav">        
         <li class="nav-item active">	    
-          <a class="nav-link" href="/podat-objavlenie">Подать объявлениe <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="/podat-objavlenie">Продать авто <span class="sr-only">(current)</span></a>
         </li>
         @if ($auth===1)
         <li class="nav-item">
